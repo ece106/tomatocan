f2bdfcc7bfe5856c9b205eacbb8edccd80c5a91f
Lisa Schaefer <rolemodelenterprises@gmail.com>
2013-06-20 17:52:29 -0400

    add signedinuser method to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -130,6 +130,10 @@ class UsersController < ApplicationController
 
   private
 
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to(signin_path) unless current_user?(@user)
