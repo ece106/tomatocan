467e0ed38d73a4cd470d8734c6a40aebd268b01e
Lisa Schaefer <rolemodelenterprises@gmail.com>
2013-06-25 12:20:37 -0400

    include SessionHelper in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+include SessionsHelper
+
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   # GET /users
