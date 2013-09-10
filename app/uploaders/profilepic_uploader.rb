70c17dd98bfd898629920bc95330e77951acf999
Lisa Schaefer <rolemodelenterprises@gmail.com>
2013-09-10 12:51:05 -0400

    enable users to upload their profilepic

--- app/uploaders/profilepic_uploader.rb
+++ app/uploaders/profilepic_uploader.rb
@@ -13,7 +13,7 @@ class ProfilepicUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
   end
 
   def cache_dir
