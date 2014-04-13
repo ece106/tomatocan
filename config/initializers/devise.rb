28b8079de001b951a6527da1bf9f21f76b60e9d2
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-04-12 20:14:42 -0400

    fixed some of the bad css

---
+++ config/initializers/devise.rb
@@ -0,0 +1,13 @@
+Devise.setup do |config|
+   config.secret_key = 'DEVISE_SECRET_KEY'
+   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+   require 'devise/orm/active_record'
+   config.case_insensitive_keys = [ :email ]
+   config.strip_whitespace_keys = [ :email ]
+   config.skip_session_storage = [:http_auth]
+   config.stretches = Rails.env.test? ? 1 : 10
+   config.reconfirmable = true
+   config.password_length = 8..128
+   config.reset_password_within = 6.hours
+   config.sign_out_via = :delete
+end
