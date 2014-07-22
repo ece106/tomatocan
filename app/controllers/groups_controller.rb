fe08854ab21ee5e41016d286ebd3a752005514de
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-07-21 23:16:38 -0400

    Sometimes the heroku database reveals the truth about my code omissions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user! , except: [:index]
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   # GET /groups
