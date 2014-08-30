67b28797f2506eb33f3e064c495f50c02f46629e
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-08-30 08:08:37 -0400

    Stupid Permit method for signup is in registrations controller not users cntlr

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     before_filter :update_sanitized_params, if: :devise_controller?
 
     def update_sanitized_params
-       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:permalink, :name, :updating_password, :email, :password, :about, :author, :password_confirmation, :remember_me, :genre1, :genre2, :genre3, :twitter, :ustreamvid, :ustreamsocial, :title, :blogurl, :profilepic, :profilepicurl, :youtube, :pinterest, :facebook)}
+       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:permalink, :name, :updating_password, :email, :password, :about, :author, :password_confirmation, :remember_me, :genre1, :genre2, :genre3, :twitter, :ustreamvid, :ustreamsocial, :title, :blogurl, :profilepic, :profilepicurl, :youtube, :pinterest, :facebook, :address, :latitude, :longitude)}
     end
 
     def localauthorsscene
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,9 +86,6 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
-    @user.address = "20016" # request.location
-#    @user.latitude = request.location.latitude
-#    @user.longitude = request.location.longitude
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
@@ -106,6 +103,8 @@ class UsersController < ApplicationController
   # POST /users.json 
   def create
     @user = User.new(user_params)
+#    @user.latitude = request.location.latitude
+#    @user.longitude = request.location.longitude
     if @user.save
       sign_in @user
       redirect_to user_profile_path(current_user.permalink)
