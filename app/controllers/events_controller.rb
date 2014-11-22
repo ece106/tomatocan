9f27c874eec6d6c12d78f6151b18b48856e27777
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-11-22 17:02:41 -0500

    Let's hope application errors stop

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,8 +7,8 @@ class EventsController < ApplicationController
       @events = Event.near(params[:search], params[:dist], order: 'distance') 
     elsif user_signed_in? && current_user.address
       @events = Event.near([current_user.latitude, current_user.longitude], 25, order: 'distance') 
-#    elsif request.location 
-#      @events = Event.near([request.location.latitude, request.location.longitude], 25, order: 'distance') 
+    elsif request.location 
+      @events = Event.near([request.location.latitude, request.location.longitude], 25, order: 'distance') 
     else
       @events = Event.near("Washington, DC", 100, order: 'distance')
     end
