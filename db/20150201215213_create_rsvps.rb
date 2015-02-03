9afd2ff556c5973faf346f891263adfc5bbb07a5
Lisa Schaefer <crowdpublishtv@gmail.com>
2015-02-03 01:03:25 -0500

    These migrations mess everything up

---
+++ db/20150201215213_create_rsvps.rb
@@ -0,0 +1,9 @@
+class CreateRsvps < ActiveRecord::Migration
+  def change
+    create_table :rsvps do |t|
+      t.integer :event_id
+      t.integer :user_id
+      t.integer :guests
+    end
+  end
+end
