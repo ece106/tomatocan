afdf69e1ad644f260d9c7bd58ce630fb80f6ec90
leslie <hi@lesliephifer.com>
2015-12-01 22:54:51 -0500

    Add unit tests for rsvpq

--- test/models/rsvpq_test.rb
+++ test/models/rsvpq_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class RsvpqTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @rsvpq = rsvpqs(:one)
+  end
+
+  [:user_id, :event_id].each do |field|
+    test "#{field}_must_not_be_empty" do
+      @rsvpq.send "#{field}=", nil
+      refute @rsvpq.valid?
+      refute_empty @rsvpq.errors[field]
+    end
+  end
 end
