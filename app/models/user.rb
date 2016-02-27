effefd33914687f7f3e82d9b443e48dcf44f382a
Lisa Schaefer <crowdpublishtv@gmail.com>
2016-02-27 14:24:40 -0500

    Check what was changed in user update geocode

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
   after_validation :geocode, :if => :address_changed?
-  after_validation :reverse_geocode, :if => :latitude_changed?
+  after_validation :reverse_geocode #, :if => :latitude_changed?
   # Other default devise modules available are:
   # :token_authenticatable, :confirmable, :lockable, :timeoutable, :validatable and :omniauthable
   scope :updated_at, -> { where(order: 'DESC') }
