98b3bd6b598759e197169d4cc2e043e468ddbf29
Lisa Schaefer <crowdpublishtv@gmail.com>
2016-04-25 19:26:24 -0400

    Correct the geocoder rescue logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -217,16 +217,19 @@ class UsersController < ApplicationController
         # primary service unreachable, try secondary...
         _page = Net::HTTP.get(URI('https://geoip-db.com/json/geoip.php'))
         lat = JSON.parse(_page)['latitude'].to_f
+        puts "RESCUE"
         puts lat
         lon = JSON.parse(_page)['longitude'].to_f
       end
       
       puts loc
 puts "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
-#      if loc.present?
-#        @user.update_attribute(:latitude, lat)
-#        @user.update_attribute(:longitude, lon)
-#      end
+      if lat.present?
+        @user.update_attribute(:latitude, lat)
+      end
+      if lon.present?
+        @user.update_attribute(:longitude, lon)
+      end
     end
 
     if @user.update_attributes(user_params)
