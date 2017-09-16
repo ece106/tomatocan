1ab50e61139ba5a699ecf1e44284b45ea9792ead
Lisa Schaefer <crowdpublishtv@gmail.com>
2017-09-16 16:37:15 -0700

    Redesign home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    userswithyoutube = User.where("LENGTH(youtube1) < 20 AND LENGTH(youtube1) > 4 ")
-    usersvidorder = userswithyoutube.order('updated_at DESC')
-    @users = usersvidorder.paginate(:page => params[:page], :per_page => 12)
+    userswithyoutube = User.where("LENGTH(youtube1) < ? AND LENGTH(youtube1) > ? AND author = ?", 20, 4, 2)
+    authorsvidorder = userswithyoutube.order('updated_at DESC')
+    @authors = authorsvidorder.paginate(:page => params[:page], :per_page => 12)
+    @authorwithpic = User.where("profilepicurl IS NOT NULL AND profilepicurl != '' ")
+    @projwithpic = Project.where("projectpic IS NOT NULL AND projectpic != '' ")
+    @groupwithpic = Group.where("grouppic IS NOT NULL AND grouppic != '' ")
   end
 
   def howwork
