b2f28d81cc068f47a721e87c7e7685a4961b9743
Lisa Schaefer <crowdpublishtv@gmail.com>
2017-10-04 12:38:56 -0700

    Phase edit buttons hide behind sidebar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,9 +5,6 @@ class StaticPagesController < ApplicationController
     userswithyoutube = User.where("LENGTH(youtube1) < ? AND LENGTH(youtube1) > ? AND author = ?", 20, 4, 'author')
     authorsvidorder = userswithyoutube.order('updated_at DESC')
     @authors = authorsvidorder.paginate(:page => params[:page], :per_page => 12)
-    @authorwithpic = User.where("profilepicurl IS NOT NULL AND profilepicurl != '' ") #I dont think we use these
-    @phswithpic = Phase.where("phasepic IS NOT NULL AND phasepic != '' ")
-    @groupwithpic = Group.where("grouppic IS NOT NULL AND grouppic != '' ")
 
     if user_signed_in?
       @orglink = groups_path
