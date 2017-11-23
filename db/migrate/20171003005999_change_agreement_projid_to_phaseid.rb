6f197a0eec8a7ba0dccdfc26f3f190dfe2b5cda2
Lisa Schaefer <crowdpublishtv@gmail.com>
2017-11-22 22:38:13 -0700

    Directly inheriting from ActiveRecord Migration is not supported

--- db/migrate/20171003005999_change_agreement_projid_to_phaseid.rb
+++ db/migrate/20171003005999_change_agreement_projid_to_phaseid.rb
@@ -1,4 +1,4 @@
-class ChangeAgreementProjidToPhaseid < ActiveRecord::Migration[4.2]
+class ChangeAgreementProjidToPhaseid < ActiveRecord::Migration[5.0]
   def change
     rename_column :agreements, :project_id, :phase_id
     rename_column :merchandises, :project_id, :phase_id
