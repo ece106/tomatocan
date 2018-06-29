9ad5cf4e4b9ce95cbb1d129d92498b0dc5facab7
Squarky <Michelle@GaylorFamily.org>
2018-06-29 23:00:02 +0000

    Added static_pages tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,41 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get aboutus" do
+    get :aboutus
+    assert_response :success
+  end
+  
+  test "should get aboutusold" do
+    get :aboutusold
+    assert_response :success
+  end 
+  
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+  
+  test "should get suggestedperks" do
+    get :suggestedperks
+    assert_response :success
+  end  
+  
+  test "should get survey" do
+    get :survey
+    assert_response :success
+  end  
+  
+  test "should get tellfriends" do
+    get :tellfriends
+    assert_response :success
+  end  
+  
+  test "should get tos" do
+    get :tos
+    assert_response :success
+  end
+
   test "correct_head" do
   	get :home
   	assert_select 'title', "CrowdPublishTV - Actors Authors - Increase Fan Engagement & Earn More Funds"
