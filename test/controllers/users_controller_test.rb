cf6e9a834fab83f7f2037c13dadb72afbbc98aca
Squarky <Michelle@GaylorFamily.org>
2018-06-29 16:38:18 +0000

    resolved conflicts

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,61 +5,24 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
 #    sign_in @user
   end
-  
-  def create
-    @user = User.new(user_params)
-#    @user.latitude = request.location.latitude
-#    @user.longitude = request.location.longitude
-
-    if @user.save
-      sign_in @user
-      redirect_to user_profile_path(current_user.permalink)
-    else
-      render 'signup'
-    end
-  end
 
   test "should get index" do
-    #throwing SQL exception
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
-  test "should create user" do 
+  test "should create user" do # To test whether address/zip from IP is saved, need to test registrations controller. But can't do on localhost.
     assert_difference('User.count', 1) do
-<<<<<<< HEAD
-<<<<<<< HEAD
-      post :create, user: { name: 'samiam', email: 'fakeunique@fake.com', password: 'secret12', 
-                    password_confirmation: 'secret12', permalink: 'samlink'  }
-    end
-    assert_redirected_to user_profile_path(assigns(:user).permalink)
-=======
-      post :create, params: { user: { name: 'samiam', email: 'fakeunique@fake.com', password: 'secret12', password_confirmation: 'secret12', permalink: 'samlink' } }
-=======
       post :create, params: { user: { name: 'samiam', email: 'fakeunique@fake.com', 
            password: 'secret12', password_confirmation: 'secret12', permalink: 'samlink' } }
->>>>>>> 2669234810cde74bbfebe105d72b90076868d7df
     end
 
     assert_redirected_to user_profileinfo_path(assigns(:user).permalink)
->>>>>>> f9c9e59abd61cfac4c7f2c3c98c5d7c54d7a4a28
   end
 
-<<<<<<< HEAD
-#  test "should create user" do # To test whether address/zip from IP is saved, need to test registrations controller. But can't do on localhost.
-#    assert_difference('User.count', 2) do
-#      post :create, user: { name: 'samiam', email: 'fakeunique@fake.com', password: 'secret12', password_confirmation: 'secret12', permalink: 'samlink'  }
-#    end
-#    assert_redirected_to user_profile_path(assigns(:user).permalink)
-#  end
-
-  test "should show user" do
-    get :show, params: { permalink: "user1" }
-=======
   test "should show user profile" do #user without any phases
     get :show, params: {permalink: 'user2' }
->>>>>>> 2669234810cde74bbfebe105d72b90076868d7df
     assert_response :success
   end
 
