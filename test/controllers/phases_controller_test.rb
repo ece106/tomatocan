d6f0ef93911165b8dfee908ff73086754127d6c7
Squarky <Michelle@GaylorFamily.org>
2018-07-04 16:23:05 +0000

    committing before pull

--- test/controllers/phases_controller_test.rb
+++ test/controllers/phases_controller_test.rb
@@ -4,7 +4,13 @@ require 'test_helper'
   setup do
     @phases = phases(:one)
   end
-  
+
+    test "should_get_phases_edit" do
+      sign_in users(:one)
+      get :edit, params: {permalink: '1dh'}
+      assert_response :success
+    end
+    
     test "should_get_phases_index" do
       get :index
       assert_response :success
@@ -16,63 +22,14 @@ require 'test_helper'
     end    
 
     test "should_get_phases_show" do
-      get :show
+      get :show, params: {permalink: '1dh'}
       assert_response :success
     end
     
     test "should_get_phases_storytellerperks" do
       sign_in users(:one)
-      get :storytellerperks
-      assert_response :success
-    end
-    
-=begin
-    test "should_get_phases_edit" do
-      sign_in users(:one)
-      @merchandise = merchandises(:one)
-      get :show, params: {permalink: '1dh'}
-      assert_response :success
-    end    
-#=begin      
-    #test whether has merchandise, if not route to storytellerperks to add something to campaign
-    #test by making users with and without merchandise (and with and without campaigns)
-
-    test "should_get_phases_edit" do
-      #@user = users(:one)
-      sign_in users(:one)
-      get :show, params: {permalink: '1dh'}
-      assert_response :success
-    end    
-    test "should_get_phases_edit" do
-      #@user = users(:one)
-      sign_in users(:one)
-      get :show, params: {permalink: '1dh'}
+      get :storytellerperks, params: {permalink: '1dh'}
       assert_response :success
     end
 
-    test "should_get_phases_edit" do
-      get :edit, params: {permalink: 'onedayinhell'}
-      assert_response :success
-    end
-      
-    test "should_get_phases_new" do
-      get :new
-      assert_response :success
-    end
-      
-    test "should_get_phases_patronperk" do
-      get :patronperk
-      assert_response :success
-    end
-      
-    test "should_get_phases_show" do
-      get :show
-      assert_response :success
-    end
-      
-    test "should_get_phases_storytellerperks" do
-      get :storytellerperks
-      assert_response :success
-    end
-=end
   end
