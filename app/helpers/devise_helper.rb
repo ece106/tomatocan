051a96d1f2973cb7f6d7b72f4e54447f88c56f86
Brigden <brigdenbryce@gmail.com>
2018-07-10 13:55:50 -0700

    Adds a page for followers and following

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -17,8 +17,8 @@ module DeviseHelper
     html.html_safe
   end
 
-  def devise_error_messages?
-    resource.errors.empty? ? false : true
-  end
+  #def devise_error_messages?
+   # resource.errors.empty? ? false : true
+  #end
 
 end
