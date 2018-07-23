587a7270163b71f61704a694180c25b507a68233
Lisa Schaefer <crowdpublishtv@gmail.com>
2018-07-23 09:29:11 -0700

    Get rid of agreement methods and views that are not used

--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -5,5 +5,5 @@ class Agreement < ApplicationRecord
   validates :phase_id, presence: true
   validates :group_id, presence: true
   validates :phase_id, uniqueness: {scope: :group_id,
-   message: 'This group has already requested to affiliate with this project phase.' }
+   message: 'This group has already requested to affiliate with this campaign.' }
 end
