6b44ff9866afdc62fd0a743fbe2c9effadeae48a
Rakshaav <rakshaavaidyanathan@gmail.com>
2018-08-17 15:38:51 -0700

    wrote test for movies controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -24,7 +24,8 @@ class MoviesControllerTest < ActionController::TestCase
 
 
   test "should show group" do
-      get :show, params: { videodesc1: 'MyString'}
+    sign_in users(:one)
+     get :show, params: { id: @movie}
     assert_response :success
   end
 
@@ -35,10 +36,10 @@ class MoviesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  
+
    test "should update group" do
     sign_in users(:one)
-    patch :update, params:{ movie: { about: @movie.about,genre: @movie.genre, moviepic: @movie.moviepic, price: @movie.price, title: @movie.title, videodesc1: @movie.videodesc1, videodesc2: @movie.videodesc2, videodesc3: @movie.videodesc3, youtube1: @movie.youtube1, youtube2: @movie.youtube2, youtube3: @movie.youtube3 } }
+    patch :update, params:{ id: @movie, movie: { about: @movie.about,genre: @movie.genre, moviepic: @movie.moviepic, price: @movie.price, title: @movie.title, videodesc1: @movie.videodesc1, videodesc2: @movie.videodesc2, videodesc3: @movie.videodesc3, youtube1: @movie.youtube1, youtube2: @movie.youtube2, youtube3: @movie.youtube3 } }
     assert_redirected_to "http://test.host/user1"
   end
 
