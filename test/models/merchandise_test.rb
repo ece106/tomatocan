44f6c6201c577a26d2a7f0dfcd22d9b0e5bacf45
Rakshaav <rakshaavaidyanathan@gmail.com>
2018-08-21 11:19:58 -0700

    Merchandise and purchses models

--- test/models/merchandise_test.rb
+++ test/models/merchandise_test.rb
@@ -4,4 +4,20 @@ class MerchandiseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+   def setup
+    @merchanise = merchandises(:one)
+  end
+
+  test "price should not be empty" do
+    merchanise = Merchandise.new
+    merchanise.send "price=",nil
+    refute merchanise.valid?
+    refute_empty merchanise.errors[:price]
+  end
+
+   test "parse youtube for merchanise" do
+      @merchanise.youtube = "http://youtube.com/watch?v=/frlviTJc"
+      @merchanise.get_youtube_id
+      refute_equal("http://youtube.com/watch?v=/frlviTJc", @merchanise.youtube)
+      end
 end
