8de48155010dcc55560358e16e89183989fa09af
Rakshaav <rakshaavaidyanathan@gmail.com>
2018-08-21 11:38:25 -0700

    Uncommented rsvpqs

--- test/controllers/rsvpqs_controller_test.rb
+++ test/controllers/rsvpqs_controller_test.rb
@@ -5,16 +5,16 @@ class RsvpqsControllerTest < ActionController::TestCase
     @rsvpq = rsvpqs(:one)
   end
 
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:rsvpqs)
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:rsvpqs)
+  end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
   test "should create rsvpq" do
     assert_difference('Rsvpq.count',1) do
@@ -23,19 +23,19 @@ class RsvpqsControllerTest < ActionController::TestCase
     assert_redirected_to events_path
   end
 
-  # test "should show rsvpq" do
-  #   get :show, id: @rsvpq
-  #   assert_response :success
-  # end
+  test "should show rsvpq" do
+    get :show, id: @rsvpq
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get :edit, id: @rsvpq
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get :edit, id: @rsvpq
+    assert_response :success
+  end
 
-  # test "should update rsvpq" do
-  #   patch :update, id: @rsvpq, rsvpq: { event_id: @rsvpq.event_id, guests: @rsvpq.guests, user_id: @rsvpq.user_id }
-  #   assert_redirected_to rsvpq_path(assigns(:rsvpq))
-  # end
+  test "should update rsvpq" do
+    patch :update, id: @rsvpq, rsvpq: { event_id: @rsvpq.event_id, guests: @rsvpq.guests, user_id: @rsvpq.user_id }
+    assert_redirected_to rsvpq_path(assigns(:rsvpq))
+  end
 
 end
