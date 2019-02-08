8ec015db3eefa5103e2d2941355e5e00e6cd4e83
Christopher <cjadam15@asu.edu>
2019-02-08 14:15:41 -0700

    All tests pass part 3

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-class AgreementsController < ApplicationController
-  before_action :set_agreement, only: [:show, :edit, :update, :destroy]
-
-  # GET /agreements
-  def index
-    @agreements = Agreement.all
-  end
-
-  # GET /agreements/1
-  def show
-  end
-
-  # POST /agreements
-  def create
-    debugger
-    @agreement = Agreement.new(agreement_params)
-    if @agreement.save
-      redirect_to phase_path(@agreement.phase_id), notice: 'Partnership request was sent.'
-    else
-      redirect_to phase_path(@agreement.phase_id), notice: 'Partnership request was not valid.'
-    end
-  end
-
-  # PATCH/PUT /agreements/1
-  def update
-    if @agreement.update(agreement_params)
-      redirect_to phase_path(@agreement.phase_id), notice: 'Partnership was updated.'
-    else
-         redirect_to phase_path(@agreement.phase_id), notice: 'Partnership was updated.'
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_agreement
-      @agreement = Agreement.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def agreement_params
-      params.require(:agreement).permit(:phase_id, :group_id)
-    end
-end
-=======
 class AgreementsController < ApplicationController
   before_action :set_agreement, only: [:show, :edit, :update, :destroy]
 
@@ -85,4 +40,3 @@ class AgreementsController < ApplicationController
       params.require(:agreement).permit(:phase_id, :group_id)
     end
 end
->>>>>>> cbf754d814f8a027947c0b2b5bfe9fd3ae9d8a06
