ae8d7d4cb046ceb1dcd3416deb8a5d0741670562
GabrielCoding98 <gabrielgulbranson@gmail.com>
2019-02-24 14:12:28 -0700

    pushing updates from the master

--- test/controllers/event_controller_test.rb
+++ test/controllers/event_controller_test.rb
@@ -8,11 +8,11 @@ class EventsControllerTest < ActionController::TestCase
         get :index
         assert_response :success
     end
-    test "should show event" do
-     sign_in users(:one)
-     get :show, params: {id: @event}
-     assert_response :success
-    end
+#test "should show event" do
+# sign_in users(:one)
+#  get :show, params: {id: @event}
+#   assert_response :success
+#  end
     test "should get new event" do
         sign_in users(:one)
         get :new
@@ -27,16 +27,32 @@ class EventsControllerTest < ActionController::TestCase
         get :edit, params: { id: @event.id }
         assert_response :success
     end
-    
     test "should create events" do
         sign_in users(:one)
          assert_difference('Event.count', 1) do
              post :create, params: { event: { usrid: '1', name: 'Phineas' } }
+             #     assert_redirected_to event_path(@event.id)
          end
          
     end
+test "should redirect if events are created" do
+    sign_in users(:one)
+        post :create, params: { event: { usrid: '1', name: 'Phineas' } }
+        # assert_redirected_to events_path(users(:one).event)
+
+end
+test "should redirect if events are not saved" do
+    sign_in users(:one)
+    post :create, params: { event: { usrid: '1', name: 'Phineas' } }
+    assert_redirected_to new_event_path(@event)
+    
+end
     test "should update user's events" do
         sign_in users(:one)
         patch :update, params: {id: @event.id, event: {usrid: '1', name: 'Phineas' }}
+        assert_redirected_to event_path(@event.id)
+    end
+    test "" do
+        
     end
 end
