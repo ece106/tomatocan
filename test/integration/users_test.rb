8afabfd7af1232eef4836941c7a69173dba19f8c
Saharaj Asa <saharaj.asa@gmail.com>
2019-03-01 16:23:09 -0700

    end of day push

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -4,4 +4,9 @@ class UsersTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "Should view profileinfo" do
+  	get '/supportourwork'
+  	assert_select 'h1', 'Discussion Hosts'
+  end
 end
