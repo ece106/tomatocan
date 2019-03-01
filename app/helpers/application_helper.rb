8340df2e8df279753af05c302579004f02652a26
Lily Martinez <lmmart42@asu.edu>
2019-03-01 13:54:57 -0700

    about us and home page edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   def flash_class(level)
       case level
-        when 'notice' then "alert alert-info"
+        when 'notice' then "alert alert-success"
         when 'success' then "alert alert-success"
         when 'error' then "alert alert-danger"
         when 'alert' then "alert alert-warning"
