caa9a3fd0bc24652ee926f7e8fc018ffaa5eaa2b
Saharaj Asa <saharaj.asa@gmail.com>
2019-03-08 11:46:50 -0700

    beginning sync

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails-controller-testing'
 
 gem 'pg'
 #gem 'sqlite3'
-
+gem 'capybara'
+gem 'selenium-webdriver'
 gem 'mandrill'	
 gem 'devise'
 gem 'fog'   #, '1.6.0'
--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
-
+#require 'capybara'
 class UsersTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
   
 	test "Should view profileinfo" do
-		get '/supportourwork'
-		assert_select 'h1', 'Discussion Hosts'
+		visit ('http://localhost:3000/')
+		click_on('Discover Discussion Hosts')
+		assert_text ('Discussion Hosts')
 	end
 	test "Should click sign up" do
-		get '/'
-		get '/signup'
-		assert_select 'h1', 'Sign Up'
+		visit ('http://localhost:3000/')
+		click_on('Sign Up', match: :first)
+		fill_in('Name', with: 'name')
+		fill_in('Email', with: 'e@mail.com')
 	end
 end
