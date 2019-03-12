a657165ab225963987902fdcd3d74d429566610e
Saharaj Asa <saharaj.asa@gmail.com>
2019-03-12 14:58:44 -0700

    another attempt at shrinking filesize

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -10,10 +10,9 @@ class UsersTest < ActionDispatch::IntegrationTest
 		click_on('Discover Discussion Hosts')
 		assert_text ('Discussion Hosts')
 	end
-	test "Should click sign up" do
+	test "Should sign up" do
 		visit ('http://localhost:3000/')
 		click_on('Sign Up', match: :first)
-		puts(page.all('input',visible: :all)) 
 		fill_in(id:'user_name', with: 'name')
 		fill_in(id:'user_email', with: 'e@gmail.com')
 		fill_in(id:'user_permalink', with:'username')
@@ -22,5 +21,19 @@ class UsersTest < ActionDispatch::IntegrationTest
 		click_on(class: 'form-control btn-primary')
 		assert_text ('Sign out')
 	end
-	
+	#todo: write a test that fails to sign up
+	test "Should see control panel" do
+		visit ('http://localhost:3000/')
+		click_on('Sign Up', match: :first)
+		fill_in(id:'user_name', with: 'name')
+		fill_in(id:'user_email', with: 'e@gmail.com')
+		fill_in(id:'user_permalink', with:'username')
+		fill_in(id:'user_password', with: 'password', :match => :prefer_exact)
+		fill_in(id:'user_password_confirmation', with:'password')
+		click_on(class: 'form-control btn-primary')
+		click_on(class: 'dropdown-toggle')
+		click_on('Control Panel')
+		assert_text('Rewards')
+	end 
+		
 end
