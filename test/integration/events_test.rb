d4303503eacf0e92dfa51bd9128d2fd0fe59214a
GabrielCoding98 <gabrielgulbranson@gmail.com>
2019-03-12 17:34:00 -0700

    check if user can join a discussion

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -14,13 +14,17 @@ class EventsTest < ActionDispatch::IntegrationTest
       fill_in(id:'user_permalink', with:'username')
       fill_in(id:'user_password', with: 'password', :match => :prefer_exact)
       fill_in(id:'user_password_confirmation', with:'password')
-      click_on(class: 'form-control btn-primary')
-      assert_text ('Profile Image')
+      click_on(class: 'form-control btn-primary')#Click Signup
+      assert_text ('Profile Image')#is user sent to their profile info page?
       visit ('http://localhost:3000/')
       click_on('Host Discussion')
-      fill_in(id:'event_name', with: 'event')
+      fill_in(id:'event_name', with: 'Intern')
       fill_in(id:'event_desc', with: 'This is a description of the event')
-      click_on(class: 'btn btn-lg btn-primary')
+      click_button('Create Event')
+      assert_text('Intern')#does the event show up on the homepage?
+      click_on('Sign out')
+      click_on('Join Discussion')
+      assert_text('Live Show')
     end
     test "Should redirect after host a discussion is clicked" do
          visit ('http://localhost:3000/')
