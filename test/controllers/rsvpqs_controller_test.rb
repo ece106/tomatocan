3935e95762e10df2a77eb4f770735b5cc204fa94
kmsarsam <kmsarsam@gmail.com>
2019-03-26 12:48:30 -0700

    trying to fix rsvpq tests

--- test/controllers/rsvpqs_controller_test.rb
+++ test/controllers/rsvpqs_controller_test.rb
@@ -6,34 +6,26 @@ class RsvpqsControllerTest < ActionController::TestCase
     @event = events(:one)
   end
 
-  # test "should get index" do
-  #   sign_in users(:one)
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:rsvpqs)
-  # end
+   test "should create rsvpq with email" do
+              post :create, params: {id: @rsvpq.id, rsvpq: { email: 'validemail@email.com' } }
 
-  # test "should get new" do
-  #   #sign_in users(:one)
-  #   get :new
-  #   assert_response :success
-  # end
+        assert_difference('Rsvpq.count', 1)
+        
+   end
 
-
-   test "should create rsvpq" do
+  test "should create rsvpq when signed in" do
     sign_in users(:one)
         assert_difference('Rsvpq.count', 1) do
         post :create, params: { rsvpq: { event_id: @event.id } }
         end
    end
 
-  #redirect to home path after successful sign in
+  #redirect to home path after successful creation
   test "should redirect to home_path" do
   sign_in users(:one)
       post :create, params: { rsvpq: { event_id: @event.id } }
       assert_redirected_to home_path
 end
-
 
 #passing
 test "should display FLASH message for invalid email" do
@@ -41,33 +33,17 @@ test "should display FLASH message for invalid email" do
   assert_equal 'Please enter a valid email address', flash[:error]
 end
 
+test "should display FLASH message for blank email" do
+  post :create, params: {id: @rsvpq.id, rsvpq: { email: '' } }
+  assert_equal 'Please enter a valid email address', flash[:error]
+end
 
 
-
-
-  #assert_redirected_to events_path
-  #assert_redirected_to "http://test.host/login"
 
   #passes
   test "should show rsvpq" do
-    sign_in users(:one)
     get :show, params:{ id: @rsvpq}
     assert_response :success
   end
-
-  # test "should get edit" do
-  #   sign_in users(:one)
-  #   get :edit
-  #   assert_response :success
-  # end
-
-  # test "should update rsvpq" do
-  #   patch :update, params:{id: @rsvpq.id, rsvpq: { event_id: @rsvpq.event_id, guests: @rsvpq.guests, user_id: @rsvpq.user_id } }
-  #   assert_redirected_to "http://test.host/login"
-  # end
-
-
-
-
 
 end
