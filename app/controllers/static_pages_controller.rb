437e5780796fa35d7598975b6cdd678316e03353
ece106 <lisa@crowdpublish.tv>
2019-04-02 17:19:42 -0700

    Because interns still cant tell that someone broke the entire home page with their useless tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 
   def home
     timenotutc = Time.now - 10.hours
-    @events = Event.where( "start_at > ?", timenotutc ).paginate(page: params[:page], :per_page => 12)
+    @events = Event.where( "start_at > ?", timenotutc ).order('start_at ASC').paginate(page: params[:page], :per_page => 12)
     if user_signed_in?
       @user = User.find(current_user.id)
     end
