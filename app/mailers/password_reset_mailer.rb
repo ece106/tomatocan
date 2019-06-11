88bec93e0cc1129344d931278403492c6b2cea33
PLgitdev <PLyssikatos@gmail.com>
2019-06-11 15:51:24 -0700

    password reset mailer changed

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,8 +1,8 @@
 class PasswordResetMailer < ApplicationMailer
   default from: "crowdpublishtv.star@gmail.com"
 
-  def password_reset
-    @user = User.find_by_reset_password_sent_at(Time.now.getutc)
+  def password_reset(user)
+    @user = user
     @url = edit_user_password_url(host: 'crowdpublish.TV')
     mail(to: @user.email, subject: "Password Reset With Crowdpublish.TV")
   end
--- test/mailers/previews/password_reset_mailer_preview.rb
+++ test/mailers/previews/password_reset_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/password_reset_mailer
 class PasswordResetMailerPreview < ActionMailer::Preview
   def password_reset
-    PasswordResetMailer.with(user: User.first).password_reset
+    PasswordResetMailer.with(user: User.first).password_reset(User.first)
   end
 end
