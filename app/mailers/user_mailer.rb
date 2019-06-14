53dad545cdf8a02ff0241fbbb582ac95521b9580
PLgitdev <PLyssikatos@gmail.com>
2019-06-14 10:31:58 -0700

    deleted more uneccessary files and folders

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,11 +7,5 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: "Welcome")
   end
 
-
-  def password_reset(user)
-    @user = user
-    @url = user_changepassword_url(@user.permalink)
-    mail(to: @user.email, subject: "Password Reset With Crowdpublish.TV")
-  end
 end
 
