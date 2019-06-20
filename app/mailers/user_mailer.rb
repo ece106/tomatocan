7465fda7abef24deaf82c5ca2ad3c377fd2e8698
PLgitdev <PLyssikatos@gmail.com>
2019-06-20 12:30:30 -0700

    Views and previews updated for mailers working previews

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,18 +5,21 @@ class UserMailer < ApplicationMailer
     @url = home_url(host:'crowdpublish.TV')
     mail(to: @user.email, subject: "Welcome")
   end
+  # These mailers could use another reason for people to come back to the site
+  # Could use suggested videos based on purchase or donation from the search feature in the future
   def purchase_saved(seller,user,purchase,merchandise)
     @seller = seller
     @user = user
     @purchase = purchase
     @merchandise = merchandise
+    @url = home_url(host: 'crowdpublish.TV')
     mail(to: @user.email, subject: 'Your purchase has been confirmed')
   end
   def donation_saved(seller,user,purchase)
     @seller = seller
     @user = user
     @purchase = purchase
+    @url = home_url(host:'crowdpublish.TV')
     mail(to: @user.email, subject: 'Your donation is appreciated')
   end
-
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,7 +4,16 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.with(user: User.first).welcome_email(User.first)
   end
   def purchase_saved
+    user = User.new(name: 'user', password: "userpassword", password_confirmation: "userpassword",
+             email: "emai@email.com", permalink: "perma")
+    merch = Merchandise.first_or_create
+    purch = Purchase.new(user: user, merchandise: merch)
+    UserMailer.purchase_saved(User.first,user,purch,merch)
   end
   def donation_saved
+    user = User.new(name: 'user', password: "userpassword", password_confirmation: "userpassword",
+                    email: "emai@email.com", permalink: "perma")
+    purch = Purchase.new(user: user)
+    UserMailer.donation_saved(User.first,user,purch)
   end
 end
