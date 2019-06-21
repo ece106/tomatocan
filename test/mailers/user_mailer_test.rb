1f3f0f9d07a53fabe32be8def725fb372eb30641
PLgitdev <PLyssikatos@gmail.com>
2019-06-21 09:21:47 -0700

    user_mailer_test updated

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,13 +4,43 @@ class UserMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
-  @user =
+
+
   test "welcome email content" do
-    mail = Usermailer.welcome_email(@user)
+    user = User.new(name: 'user', password: "userpassword",
+             password_confirmation: "userpassword",
+             email: "email@email.com", permalink: "perma")
+    mail = UserMailer.welcome_email(user)
     assert_equal "Welcome", mail.subject
-    assert_equal "" ,mail.to
-    assert_equal "", mail.from
-    assert_equal "", mail.body.encoded
+    assert_equal [user.email] ,mail.to
+    assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
 
   end
+  test 'purchase saved email content' do
+    user = User.new(name: 'user', password: "userpassword",
+                    password_confirmation: "userpassword",
+                    email: "email@email.com", permalink: "perma")
+    seller = User.first_or_create
+    purchase = Purchase.new(user: user)
+    merchandise = Merchandise.first_or_create
+    mail = UserMailer.purchase_saved(seller,user,purchase,merchandise)
+    assert_equal "Your purchase has been confirmed", mail.subject
+    assert_equal [user.email] ,mail.to
+    assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
+
+  end
+  test 'donation saved email content' do
+    user = User.new(name: 'user', password: "userpassword",
+                     password_confirmation: "userpassword",
+                     email: "email@email.com", permalink: "perma")
+    seller = User.first_or_create
+    purchase = Purchase.new(user: user)
+
+    mail = UserMailer.donation_saved(seller,user,purchase)
+    assert_equal 'Your donation is appreciated', mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
+
+  end
+
 end
