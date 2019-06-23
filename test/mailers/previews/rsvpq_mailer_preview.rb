58b688d13fa7149c362539c743ce78ecc3677a97
codykarunas <codykarunas@gmail.com>
2019-06-23 16:56:29 -0400

    Added a hash to pass in mailer parameters when calling the parameterized RsvpqMailer. Fixed indentation adn removed whitespaces.

--- test/mailers/previews/rsvpq_mailer_preview.rb
+++ test/mailers/previews/rsvpq_mailer_preview.rb
@@ -1,7 +1,8 @@
 class RsvpqMailerPreview < ActionMailer::Preview
 
   def rsvpq_created
-    RsvpqMailer.with(rsvpq: Rsvpq.find_by(event_id: 4), user: User.first).rsvpq_created
+    rsvpq_mailer_hash = { rsvpq: Rsvpq.find_by(event_id: 4), user: User.first }
+    RsvpqMailer.with(rsvpq_mailer_hash).rsvpq_created
   end
 
 end
