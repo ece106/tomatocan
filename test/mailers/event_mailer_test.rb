fd54f475ac91b96b71e27f3f83d355c818d41e7a
PLgitdev <PLyssikatos@gmail.com>
2019-06-24 15:39:14 -0700

    tests fixed

--- test/mailers/event_mailer_test.rb
+++ test/mailers/event_mailer_test.rb
@@ -4,11 +4,12 @@ class EventMailerTest < ActionMailer::TestCase
   setup do
     @user = users(:two)
     @recipient = users(:one)
+    @mail_hash = {recipient:@recipient,event:events(:one),user:@user}
   end
   test "new_event mail to,from,subject" do
 
     event = Event.first_or_create
-    mail = EventMailer.new_event(@recipient,event,@user)
+    mail = EventMailer.with(@mail_hash).new_event
     assert_equal "Someone you follow has created an event", mail.subject
     assert_equal [@recipient.email], mail.to
     assert_equal ["crowdpublishtv.star@gmail.com"], mail.from
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,25 +7,22 @@ class UserMailerTest < ActionMailer::TestCase
   setup do
     @user = users(:one)
     @seller = users(:two)
+    @mail_hash = {seller:@seller, user: users(:one), purchase:purchases(:one)}
   end
-  test "welcome email to,from,subject" do
-
-    mail = UserMailer.welcome_email(@user)
+  test 'welcome email to,from,subject' do
+    mail = UserMailer.with(user: @user).welcome_email
     assert_equal "Welcome", mail.subject
     assert_equal [@user.email] ,mail.to
     assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
   end
   test 'purchase saved email to,from,subject' do
-    purchase = Purchase.new(user: @user)
-    merchandise = Merchandise.first_or_create
-    mail = UserMailer.purchase_saved(@seller,@user,purchase,merchandise)
-    assert_equal "Your purchase has been confirmed", mail.subject
-    assert_equal [@user.email] ,mail.to
+    mail = UserMailer.with(user:@user,seller:@seller,purchase:purchases(:one),merchandise:merchandises(:one)).purchase_saved
+    assert_equal 'Your purchase has been confirmed', mail.subject
+    assert_equal [@user.email],mail.to
     assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
   end
   test 'donation saved email to,from,subject' do
-    purchase = Purchase.new(user: @user)
-    mail = UserMailer.donation_saved(@seller,@user,purchase)
+    mail = UserMailer.with(@mail_hash).donation_saved
     assert_equal 'Your donation is appreciated', mail.subject
     assert_equal [@user.email], mail.to
     assert_equal ['crowdpublishtv.star@gmail.com'], mail.from
