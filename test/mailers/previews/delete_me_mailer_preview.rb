8b69db4a339d226dbfdd1152a5b6685f466cf5f7
codykarunas <codykarunas@gmail.com>
2019-06-27 10:41:30 -0400

    Removed an example mailer I generated.

--- test/mailers/previews/delete_me_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/delete_me_mailer
-class DeleteMeMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/delete_me_mailer/delete_me_method
-  def delete_me_method
-    DeleteMeMailer.delete_me_method
-  end
-
-end
