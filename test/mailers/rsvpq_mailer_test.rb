f7561f0a5abb432ea454efe6d1e40802f63c831e
codykarunas <codykarunas@gmail.com>
2019-06-27 15:09:53 -0400

    Added assertion values to test values.

--- test/mailers/rsvpq_mailer_test.rb
+++ test/mailers/rsvpq_mailer_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class RsvpqMailerTest < ActionMailer::TestCase
   setup do
+    @rsvpq = rsvpqs(:one)
+    @user  = users(:one)
   end
 
   test "rsvpq_created" do
-    mail = RsvpqMailer.rsvpq_created
+    mail = RsvpqMailer.with(rsvpq: @rsvpq, user: @user).rsvpq_created
 
-    assert_equal "Rsvpq created", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-
-    assert_match "Hi", mail.body.encoded
+    assert_equal "Successfully created an RSVP.",   mail.subject
+    assert_equal ["fake@fake.com"],                 mail.to
+    assert_equal ["crowdpublishtv.star@gmail.com"], mail.from
   end
 
 end
