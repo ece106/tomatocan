9180f47768cba49382eb3b12222d2121d9371fe0
prachiMiyan <pmiyan@asu.edu>
2019-06-28 13:54:29 -0700

    refactored the code and worked on further testing purchases section of the application

--- test/integration/purchases_test.rb
+++ test/integration/purchases_test.rb
@@ -16,25 +16,6 @@ class UsersTest < ActionDispatch::IntegrationTest
     setup do
     visit ('http://localhost:3000/')
 
-  def signUpPurchaser()
-      visit ('http://localhost:3000/')
-      click_on('Sign Up', match: :first)
-      fill_in(id:'user_name', with: 'purchaser')
-      fill_in(id:'user_email', with: 'pur@gmail.com')
-      fill_in(id:'user_permalink', with:'purchaser')
-      fill_in(id:'user_password', with: 'password', :match => :prefer_exact)
-      fill_in(id:'user_password_confirmation', with:'password')
-      click_on(class: 'form-control btn-primary')
-      click_on('Sign out')
-  end
-  def signInPurchaser()
-      visit ('http://localhost:3000/')
-      click_on('Sign In', match: :first)
-      fill_in(id:'user_email', with: 'pur@gmail.com')
-      fill_in(id:'user_password', with: 'password')
-      click_on(class: 'form-control btn-primary')
-  end
-
   def signUpFixture()
     visit ('http://localhost:3000/')
     click_on('Sign Up', match: :first)
@@ -57,42 +38,7 @@ end
         @time=Time.now
     end
 
-    test 'Should_buy_from_user_error' do #card source not present
-        signUpPurchaser() #signing up a new user
-
-        cardToken = Stripe::Token.create({  #create Stripe account for new user
-          card: {
-            number: "4242424242424242",
-            exp_month: 1,
-            exp_year: 2023,
-            cvc: "123"
-          }
-        })
-        customer = Stripe::Customer.create(
-                                          :source => cardToken,
-                                          :description => 'purchaser',
-                                          :email => 'pur@gmail.com'
-                                          )
-        customer.save
-
-        signInPurchaser()
-        click_on('Discover Previous Discussions')
-        click_link(@seller.name)
-        puts(@seller.name + " seller name")
-        click_on('Buy for $1.50!')
-        puts ("clicked")
-        #fill_in(id:'purchase_email', with:'pur@gmail.com')
-        fill_in(id:'card_number', with:'4242424242424242')
-        fill_in(id:'card_code', with:'123')
-        #select('1 - January', from: 'card_month')
-        select('2023', from: 'card_year')
-        click_on('Purchase')
-
-        assert_text 'Your order did not go through. Try again.'
-    end
-
-    test 'signing up a fixture' do
-      puts @purchaser.encrypted_password
+    test "signing up a fixture" do
       signUpFixture()
       assert_text('Email has already been taken')
     end
@@ -114,8 +60,6 @@ end
                                         )
       customer.save
       @purchaser.update_column(:stripe_customer_token, customer.id)
-      puts (@purchaser.stripe_customer_token)
-      
       signInFixture()
       puts("signed in")
       click_on('Discover Previous Discussions')
@@ -147,15 +91,11 @@ end
                                         )
       customer.save
       @purchaser.update_column(:stripe_customer_token, customer.id)
-      puts (@purchaser.stripe_customer_token)
-      
       signInFixture()
-      puts("signed in")
       click_on('Discover Previous Discussions')
       click_link(@seller.name)
       puts(@seller.name + " seller name")
       click_on('Donate $2.00!', match: :first)
-      puts ("clicked")
       within '.buyexistingcard' do
         click_button('Buy now')
       end
@@ -163,17 +103,34 @@ end
       assert_text 'You have successfully completed the purchase!'
     end
 
-    test "to_purchase_donation_by_a_fixture_whose_card_is_not_registered_with_stripe" do
+    test "to_purchase_merchandise_without_a_payment_method" do
+        puts "test 1"
         signInFixture()
-        puts("signed in")
         click_on('Discover Previous Discussions')
         click_link(@seller.name)
-        puts(@seller.name + " seller name")
-        click_on('Donate $2.00!', match: :first)
-        puts ("clicked")
-
+        click_on('Buy for $1.50')
         click_on('Purchase')
-        assert_text 'Could not find payment information'
+        assert_text 'Your order did not go through. Try again.'
     end
+
+
+    test "to_purchase_a_merchandise_with_card" do #card source not present
+      puts "test 2"
+      signInFixture()
+      click_on('Discover Previous Discussions')
+      click_link(@seller.name)
+      click_on('Buy for $1.50!')
+      page.fill_in 'card_number', with:"4242424242424242"
+      page.fill_in 'card_code', with:"123"
+      #find(:css, "input[id$='card_number']").set("4242424242424242")
+      #find(:css, "input[id$='card_code']").set("123")
+      #page.find_field('card_number').send_keys("4242424242424242")
+      #page.find_field('card_code').send_keys("123")
+      select('2023', from: 'card_year')
+      select('1 - January', from: 'card_month')
+      click_on('Purchase')
+      assert_text 'You have successfully completed the purchase!'
+      #stripe error, source not present? "JavaScript is not enabled and is required for this form. First enable it in your web browser settings."
+  end
 
 end
