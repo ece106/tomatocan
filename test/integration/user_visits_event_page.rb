8f8eedfbf1b220c8ba74a32a0d9c4882355a0bcd
codykarunas <codykarunas@gmail.com>
2019-07-04 13:46:50 -0400

    Fixed indentation and a missing div in events show page. Added more test for user visits event page integration.

--- test/integration/user_visits_event_page.rb
+++ test/integration/user_visits_event_page.rb
@@ -8,7 +8,9 @@ class UserVisitsEventPage < ActionDispatch::IntegrationTest
     @event = events(:one)
     @rsvpq = rsvpqs(:one)
 
-    visit event_path(@event)
+    sign_in_as @user
+
+    visit event_path @event
   end
 
   test "visits event page successfully" do
@@ -20,31 +22,57 @@ class UserVisitsEventPage < ActionDispatch::IntegrationTest
   end
 
   test "should see the correct event title" do
-    page.has_content?(@event.name)
+    assert page.has_content? @event.name
   end
 
   test "should see the correct event host" do
-    page.has_content?(@user.name)
+    assert page.has_content? @user.name
   end
 
-  # TODO: Coming back to this
-  test "should see the edit_event link" do
-    page.has_link?(edit_event_path(@event))
-    # page.click_on(edit_event_path(@event))
-  end
+  # TODO: coming back to this
+  # test "should see the edit event link" do
+  #   assert page.has_link?("Edit Show")
+  # end
 
   test "should see the correct start date" do
-    page.has_content?(@event.start_at.strftime("%B %d, %Y"))
+    assert page.has_content? @event.start_at.strftime("%A, %B %d")
   end
 
   test "should see the correct start at timezones" do
-    page.has_content?(@event.start_at.strftime("%I:%M %p"))
-    page.has_content?((@event.start_at + 3).strftime("%I:%M %p"))
+    assert page.has_content?(@event.start_at.strftime("%I:%M %p"))
+    assert page.has_content?((@event.start_at + 3.hours).strftime("%I:%M %p"))
   end
 
-  test "should see the correct duration" do
-    # @duration = ((@event.end_at - @event.start_at) / 60).floor
-    page.has_css?(".event-duration")
+  test "can join live discussion successfully" do
+    click_on "Join Live Discussion"
+  end
+
+  test "can join live show successfully" do
+    click_on "Join Live Show"
+  end
+
+  test "can share event" do
+    page.has_css? "#shareBtn"
+
+    click_on id: "shareBtn"
+
+    assert page.has_xpath? "//img[@src = 'https://static.licdn.com/sc/h/eahiplrwoq61f4uan012ia17i' and @alt='LinkedIn']"
+    assert page.has_xpath? "//img[@src = 'https://www.facebook.com/images/fb_icon_325x325.png' and @alt='Facebook']"
+    assert page.has_xpath? "//img[@src = 'https://about.twitter.com/etc/designs/about-twitter/public/img/apple-touch-icon-72x72.png' and @alt='Twitter']"
+  end
+
+  test "can make an rsvp for event" do
+    click_on id: "RSVPsubmit"
+  end
+
+  private
+
+  def sign_in_as user
+    visit root_path
+    click_on 'Sign In', match: :first
+    fill_in id: 'user_email', with: user.email
+    fill_in id: 'user_password', with: user.encrypted_password
+    click_on class: 'form-control btn-primary'
   end
 
 end
