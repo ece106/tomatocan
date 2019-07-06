890cd49ba2bc95c445a776e512ce1d7f454736e6
PLgitdev <PLyssikatos@gmail.com>
2019-07-06 15:19:44 -0700

    removed email followers

--- test/mailers/event_mailer_test.rb
+++ test/mailers/event_mailer_test.rb
@@ -6,11 +6,6 @@ class EventMailerTest < ActionMailer::TestCase
     @recipient = users(:one)
     @mail_hash = {recipient:@recipient,event:events(:one),user:@user}
   end
-  test "new_event mail to,from,subject" do
-    mail = EventMailer.with(@mail_hash).new_event
-    assert_equal "Someone you follow has created an event", mail.subject
-    assert_equal [@recipient.email], mail.to
-    assert_equal ["crowdpublishtv.star@gmail.com"], mail.from
-  end
+
 
 end
