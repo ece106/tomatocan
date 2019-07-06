62a30eb7c40491b85b932b575f3a70f1cc06dda1
PLgitdev <PLyssikatos@gmail.com>
2019-07-06 15:38:39 -0700

    removed new event from mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -11,11 +11,6 @@ class EventMailer < ApplicationMailer
   before_action :set_url , only: [:new_event,:event_reminder]
   before_action :format_date, only: [:event_reminder]
 
-  def new_event
-    mail(to: @recipient.email, subject: "Someone you follow has created an event")
-
-  end
-
   def event_reminder
     mail(to: @user.email, subject: "A reminder for your event on #{@date_subject_format}")
   end
