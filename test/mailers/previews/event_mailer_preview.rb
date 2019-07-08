3c2c0cb3874b718947565957ebca10da61ff5fad
PLgitdev <PLyssikatos@gmail.com>
2019-07-08 16:45:26 -0700

    css for mailer views

--- test/mailers/previews/event_mailer_preview.rb
+++ test/mailers/previews/event_mailer_preview.rb
@@ -1,9 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/event_mailer
 class EventMailerPreview < ActionMailer::Preview
 
-  def new_event
-    EventMailer.with(user: User.first, event: Event.first_or_create, recipient: User.last).new_event
-  end
   def event_reminder
     event = Event.create(start_at: DateTime.now, name: 'event in 3 days')
     EventMailer.with(user: User.first, event: event).event_reminder
