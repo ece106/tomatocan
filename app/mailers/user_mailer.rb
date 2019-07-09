fde7eeddf593c0f7296ad88c66535f598033170c
PLgitdev <PLyssikatos@gmail.com>
2019-07-08 17:14:34 -0700

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
  
-	before_action do
+  before_action do
     @user = params[:user]
     @seller = params[:seller]
     @purchase = params[:purchase]
@@ -8,8 +8,8 @@ class UserMailer < ApplicationMailer
   end
 
   before_action :set_url
-  
-	def welcome_email
+
+  def welcome_email
     mail(to: @user.email, subject: "Welcome")
   end
   # purchase and donation saved could use another reason for people to come back to the site
