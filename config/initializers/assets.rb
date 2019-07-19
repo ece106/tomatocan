a2fcaf8c8470e24c46a06d2d53011ca863d3b260
ck496 <chriskurian24@gmail.com>
2019-07-18 22:39:41 -0700

    ThinQ.tv

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( _faq.scss )
+Rails.application.config.assets.precompile += %w( aboutus.scss )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
