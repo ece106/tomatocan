cb0e0e249e089c09d8776c1dc5f13d03151ea748
wujason1 <jasondewulo@gmail.com>
2019-07-27 18:11:12 -0700

    Useful Comment of Your Changes/Additions

---
+++ config/initializers/aakeys.rb
@@ -0,0 +1,10 @@
+DEVISE_SECRET_KEY = 'fake'
+AWS_KEY = 'morefake'
+AWS_SECRET_KEY = 'pretend' 
+AWS_BUCKET = 'yourawsbucketname'
+STRIPE_SECRET_KEY = "sk_test_rzuF84dh09A3UQqG3qyp9wSy"
+STRIPE_PUBLIC_KEY = "pk_test_4BXQdR5LWhJu8R44hwAjJJxk"
+GMAIL_PWD = "superfake"
+Stripe.api_key = STRIPE_SECRET_KEY
+STRIPE_CONNECT_CLIENT_ID = "ca_6Ws7cje29FmtiJa6Lwh9DcxsehbXBC4Q"
+
