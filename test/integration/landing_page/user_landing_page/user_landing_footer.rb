8866add422eaabadfd17264f08e70fcefcb52a1b
tqingASU <tqing@asu.edu>
2019-08-29 12:52:53 -0700

    Wrote out all of the test files that I could. However have a lot of errors so will take a while to fix

--- test/integration/landing_page/user_landing_page/user_landing_footer.rb
+++ test/integration/landing_page/user_landing_page/user_landing_footer.rb
@@ -10,31 +10,30 @@ class UserLandingFooter < ActionDispatch::IntegreationTest
     visit root_path
 	end
 	
-	test 'About link' do
+	test 'information and mail links' do
+		assert page.has_class? 'f-link'
+		click_on class: 'f-link'
 		
-	end
-	
-	test 'FAQ link' do
+		about_link = find('link=About')
+		faq_link = find('link=FAQ')
+		tos_link = find('link=Terms of Service')
+		mail_link = find('link=info@ThinQ.tv')
 		
-	end
-	
-	test 'Terms of Service link' do
-		
-	end
-	
-	test 'contact email link' do
-		
+		assert page.has_xpath? about_link
+		assert page.has_xpath? faq_link
+		assert page.has_xpath? tos_link
+		assert page.has_xpath? mail_link
 	end
 	
 	test 'share site with others' do
-		page.has_class? 'social-share-button'
+		assert page.has_class? 'social-share-button'
 		
 		click_on class: 'social-share-button'
 		
-		facebook_img = 
-		twitter_img = 
-		linkedin_img = 
-		email_img = 
+		facebook_img = "//[@src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.3']"
+		twitter_img = "//[@src = 'https://twitter.com/intent/tweet?hashtags=ThinQTV&text=Broadcast%20live%20discussions%20about%20what%20you%27re%20doing%20to%20change%20the%20world%20and%20participate%20in%20intellectual%20discussions%20to%20broaden%20your%20knowledge!&tw_p=tweetbutton&url=http%3A%2F%2Fwww.ThinQ.tv%2F']"
+		linkedin_img = "//[@src = 'https://www.linkedin.com/shareArticle?mini=true&url=']"
+		email_img = "//[@src = 'mailto:?subject=Check out this site called ThinQTV!&amp;body=You are invited to check out <%= url %>!%0D%0A%0D%0A<%= desc %>%0D%0A%0D%0AThe schedule is: Tuesdays 6:30pm PDT / 9:30pm EDT - Tech Tuesday%0D%0AWednesdays 6:30pm PDT / 9:30pm EDT - Conversations on Faith%0D%0AThursdays 7:30pm PDT / 10:30pm EDT - International Awareness%0D%0A%0D%0AStay tuned for more weekly topics on Books, Innovation, and Environmental issues.%0D%0A%0D%0A%0D%0A%0D%0A']"
 		
 		assert page.has_xpath? facebook_img
 		assert page.has_xpath? twitter_img
