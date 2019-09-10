bb29479287a48a55fc3d20cc6826e21cb0d92020
tqingASU <tqing@asu.edu>
2019-09-10 13:37:55 -0700

    fixed the nonuser_navbar_test.rb file, so no errors appear. However must wait for Peter's Sign In thing to be pulled to the main tomatocan repository so i can change my document to fit that.

--- test/integration/navbar/nonuser_navbar_test.rb
+++ test/integration/navbar/nonuser_navbar_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 require "capybara-screenshot/minitest"
 require "pry"
+require "securerandom"
 
 class NonuserNavbar < ActionDispatch::IntegrationTest
 	setup do
@@ -27,28 +28,46 @@ class NonuserNavbar < ActionDispatch::IntegrationTest
 		assert page.has_link? 'Sign Up'
 		find_link('Sign Up', match: :first).click
 		assert_equal '/signup', current_path
-
+		sign_up
+		assert_equal '/fake_user/profileinfo', current_path
 	end
 
 	test 'sign in' do
-		binding.pry
 		assert page.has_css? '.navbar-btn'
     assert page.has_link? 'Sign In'
 		find_link('Sign In', match: :first).click
     assert_equal '/login', current_path
-		sign_in
+		sign_in @user
 		assert_equal '/', current_path
+		click_on class: 'btn btn-default', match: :first
 	end
   
 	private
-  def sign_in
-    visit root_path users
+	def sign_up
+		visit root_path
 
-    click_on 'Sign In', match: :first
+		click_on class: 'btn btn-primary'
+		
+		fill_in id: 'user_name', with: "fake_name"
+		fill_in id: 'user_email',    with:  "newfake@fake.com"
+		fill_in id: 'user_permalink', with: "fake_user"
+    fill_in id: 'user_password', with: "fake_password"
+		fill_in id: 'user_password_confirmation', with: "fake_password"
 
-    fill_in id: 'user_email', with: "#{@user.email}"
-    fill_in id: 'user_password', with: "#{@user.password}"
+		click_on class: 'form-control btn-primary'
+	end
 
-    click_on class: 'form-control btn-primary'
+  def sign_in user
+    visit root_path 
+			
+		click_on class: 'btn btn-default'
+		
+		fill_in id: 'user_email',    with:  "#{user.email}"
+		fill_in id: 'user_password', with:  "user1234"
+		
+		click_on class: 'form-control btn-primary'
   end
+	
+	def teardown 
+	end
 end
