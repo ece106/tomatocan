0ca30bed336e36608743ce1b94a8c320c9c27fb6
tqingASU <tqing@asu.edu>
2019-09-17 12:13:51 -0700

    Updated some of the files for the navbar tests. Need to change to test branch so just to be safe.

--- test/integration/navbar/nonuser_navbar_test.rb
+++ test/integration/navbar/nonuser_navbar_test.rb
@@ -3,71 +3,70 @@ require "capybara-screenshot/minitest"
 require "pry"
 
 class NonuserNavbar < ActionDispatch::IntegrationTest
-	setup do
-		@user = users :one
-		@event = events :one
-		
-		visit root_path	
-	end
-	
-	test 'navitem buttons' do 
-		assert page.has_css? '.nav-item'
-		find_link('Home', match: :first).click
-		assert_equal '/', current_path
-		find_link('About', match: :first).click
-		assert_equal '/aboutus', current_path
-		find_link('Discover Previous Conversations', match: :first).click
-		assert_equal '/supportourwork', current_path 
-		find_link('FAQ', match: :first).click
-		assert_equal '/faq', current_path
-	end
+  setup do
+    @user = users :one
+    
+    visit root_path 
+  end
   
-	test 'sign up' do
-		assert page.has_css? '.navbar-btn'
-		assert page.has_link? 'Sign Up'
-		find_link('Sign Up', match: :first).click
-		assert_equal '/signup', current_path
-		sign_up
-		assert_equal '/fake_user/profileinfo', current_path
-		click_on class: 'btn btn-default', match: :first
-	end
+  test 'navitem buttons' do 
+    assert page.has_css? '.nav-item'
+    find_link('Home', match: :first).click
+    assert_equal '/', current_path
+    find_link('About', match: :first).click
+    assert_equal '/aboutus', current_path
+    find_link('Discover Previous Conversations', match: :first).click
+    assert_equal '/supportourwork', current_path 
+    find_link('FAQ', match: :first).click
+    assert_equal '/faq', current_path
+  end
+  
+  test 'sign up' do
+    assert page.has_css? '.navbar-btn'
+    assert page.has_link? 'Sign Up'
+    find_link('Sign Up', match: :first).click
+    assert_equal '/signup', current_path
+    sign_up
+    assert_equal '/fake_user/profileinfo', current_path
+    click_on class: 'btn btn-default', match: :first
+  end
 
-	test 'sign in' do
-		assert page.has_css? '.navbar-btn'
+  test 'sign in' do
+    assert page.has_css? '.navbar-btn'
     assert page.has_link? 'Sign In'
-		find_link('Sign In', match: :first).click
+    find_link('Sign In', match: :first).click
     assert_equal '/login', current_path
-		sign_in @user
-		assert_equal '/', current_path
-		click_on class: 'btn btn-default', match: :first
-	end
+    sign_in @user
+    assert_equal '/', current_path
+    click_on class: 'btn btn-default', match: :first
+  end
   
-	private
-	def sign_up
-		visit root_path
+  private
+  def sign_up
+    visit root_path
 
-		click_on class: 'btn btn-primary'
-		
-		fill_in id: 'user_name', with: "fake_name"
-		fill_in id: 'user_email',    with:  "newfake@fake.com"
-		fill_in id: 'user_permalink', with: "fake_user"
+    click_on class: 'btn btn-primary'
+    
+    fill_in id: 'user_name', with: "fake_name"
+    fill_in id: 'user_email',    with:  "newfake@fake.com"
+    fill_in id: 'user_permalink', with: "fake_user"
     fill_in id: 'user_password', with: "fake_password"
-		fill_in id: 'user_password_confirmation', with: "fake_password"
+    fill_in id: 'user_password_confirmation', with: "fake_password"
 
-		click_on class: 'form-control btn-primary'
-	end
+    click_on class: 'form-control btn-primary'
+  end
 
   def sign_in users
     visit root_path 
-			
-		click_on class: 'btn btn-default'
-		
-		fill_in id: 'user_email',    with:  "#{users.email}"
-		fill_in id: 'user_password', with:  "user1234"
-		
-		click_on class: 'form-control btn-primary'
+      
+    click_on class: 'btn btn-default'
+    
+    fill_in id: 'user_email',    with:  "#{users.email}"
+    fill_in id: 'user_password', with:  "user1234"
+    
+    click_on class: 'form-control btn-primary'
   end
-	
-	def teardown 
-	end
+  
+  def teardown 
+  end
 end
--- test/integration/navbar/user_navbar_test.rb
+++ test/integration/navbar/user_navbar_test.rb
@@ -3,16 +3,15 @@ require "capybara-screenshot/minitest"
 require "pry"
 
 class UserNavbar < ActionDispatch::IntegrationTest
-	setup do
-		@user = users :one
-		@event = events :one
-		
-		sign_in @user
-		
-		visit root_path	
-	end
+  setup do
+    @user = users :one
+    
+    sign_in @user
+    
+    visit root_path 
+  end
   
-	test 'navitem buttons' do
+  test 'navitem buttons' do
     assert page.has_css? '.nav-item'
     find_link('Home', match: :first).click
     assert_equal '/', current_path
@@ -23,50 +22,49 @@ class UserNavbar < ActionDispatch::IntegrationTest
     find_link('FAQ', match: :first).click
     assert_equal '/faq', current_path
   end
-	
-	test 'view profile page' do
-		assert text, "#{@user.name}"
-		assert page.has_css? '.dropdown'
-		assert page.has_css? '.dropdown-toggle'
-		find(class: 'dropdown-toggle',match: :first).click
+  
+  test 'view profile page' do
+    assert text, "#{@user.name}"
+    assert page.has_css? '.dropdown'
+    assert page.has_css? '.dropdown-toggle'
+    find(class: 'dropdown-toggle',match: :first).click
 
-		assert page.has_link? 'View Profile'
-		find_link('View Profile',match: :first).click
-		assert_equal "/#{@user.permalink}", current_path
-	end
-	
-	test 'view control panel page' do
-		assert text, "#{@user.name}"
-		assert page.has_css? '.dropdown'
-		assert page.has_css? '.dropdown-toggle'
-		find(class: 'dropdown-toggle',match: :first).click
+    assert page.has_link? 'View Profile'
+    find_link('View Profile',match: :first).click
+    assert_equal "/#{@user.permalink}", current_path
+  end
+  
+  test 'view control panel page' do
+    assert text, "#{@user.name}"
+    assert page.has_css? '.dropdown'
+    assert page.has_css? '.dropdown-toggle'
+    find(class: 'dropdown-toggle',match: :first).click
 
     assert page.has_link? 'Control Panel'
-		find_link('Control Panel',match: :first).click
-		#ask about the user.permalink thing not working below
-		assert_equal "/#{@user.permalink}/controlpanel", current_path
-	end
-		
-	test 'logout successfully' do
-		#assert page.has_css? 'btn btn-default'
-		click_on class: 'btn btn-default', match: :first
-		
-		assert '/', current_path
-	end
+    find_link('Control Panel',match: :first).click
+    assert_equal "/#{@user.permalink}/controlpanel", current_path
+  end
+    
+  test 'logout successfully' do
+    #assert page.has_css? 'btn btn-default'
+    click_on class: 'btn btn-default', match: :first
+    
+    assert '/', current_path
+  end
 
-	private
-	
-	def sign_in users
-		visit root_path
+  private
+  
+  def sign_in users
+    visit root_path
 
-		click_on class: 'btn btn-default'
+    click_on class: 'btn btn-default'
 
-		fill_in id: 'user_email', with: "#{users.email}"
-		fill_in id: 'user_password', with: "user1234"
+    fill_in id: 'user_email', with: "#{users.email}"
+    fill_in id: 'user_password', with: "user1234"
 
-		click_on class: 'form-control btn-primary'
-	end
-	
-	def teardown
-	end
+    click_on class: 'form-control btn-primary'
+  end
+  
+  def teardown
+  end
 end
