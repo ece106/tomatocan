0b3a923e470b6ecaac1eab543747fe47d0e669e2
ece106 <lisa@crowdpublish.tv>
2019-10-25 11:51:53 -0700

    Google analytics for new google account

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -36,9 +36,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-74064103-1', 'auto');
-  ga('send', 'pageview');
-
   ga('create', 'UA-150909386-1', 'auto');
   ga('send', 'pageview');
 
