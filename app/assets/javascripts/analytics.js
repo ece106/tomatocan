7972b215e62ee0eedc710c9c8d4c79bfa51bf452
ece106 <lisa@crowdpublish.tv>
2019-10-25 15:19:10 -0700

    Google analytics for new google account

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -31,23 +31,17 @@
     }
     OneStat_Pageview();
 
+  (function(i,s,o,g,r,a,m)
+    {i['GoogleAnalyticsObject']=r;i[r]=i[r]||function()
+      {(i[r].q=i[r].q||[]).push(arguments)},
+      i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];
+      a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })
+  (window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-150909386-1');
-
-
-
-//  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-//  ga('create', 'UA-150909386-1', 'auto');
-//  ga('send', 'pageview');
-
+  ga('create', 'UA-150909386-1', 'auto');
+  ga('send', 'pageview');
 
  // Check if current url is a profileinfo or edit page. urlArray[3] == "stories" &&
 
