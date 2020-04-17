0c1525c1d807cdc0782f3705810186f9914f45b6
honestabej <honestabej@hotmail.com>
2020-04-17 15:04:20 -0700

    reverting files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,4 @@
 class User < ApplicationRecord
-  
-  # Facebook login
-  devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable,
-    :confirmable, :lockable, :timeoutable,
-    :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2, :twitter] 
-  def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
-      user.email = provider_data.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.skip_confirmation!
-    end
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
-  
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-    end
-  end
-  # End of Facebook login
-
   acts_as_token_authenticatable
 
   attr_accessor :monthperkinfo, :monthbookinfo, :incomeinfo, :salebyfiletype, :salebyperktype, :totalinfo, 
