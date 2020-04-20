5169c01258894c2f97d22569645b376364b4593d
Karl <karlrobin_espinosa@yahoo.com>
2020-04-20 14:14:42 -0700

    Added some comments

--- db/migrate/20200419233416_create_banned_users.rb
+++ db/migrate/20200419233416_create_banned_users.rb
@@ -1,13 +1,17 @@
 class CreateBannedUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :banned_users do |t|
+      # user id of banned user
       t.integer :user
+      # id of event they're banned from
       t.integer :host
 
       t.timestamps
     end
 
+    # user column references id column of users table
     add_foreign_key :banned_users, :users, column: :user, primary_key: "id"
+    # host column references id column of events table
     add_foreign_key :banned_users, :events, column: :host, primary_key: "id"
   end
 end
