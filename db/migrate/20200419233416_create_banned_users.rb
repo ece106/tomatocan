ec9cc249c4143ba99ec6c83ec5c57380a30999d0
Karl <karlrobin_espinosa@yahoo.com>
2020-04-19 17:08:03 -0700

    Updated foreign key reference of host column to primary key of events table.

--- db/migrate/20200419233416_create_banned_users.rb
+++ db/migrate/20200419233416_create_banned_users.rb
@@ -8,6 +8,6 @@ class CreateBannedUsers < ActiveRecord::Migration[5.2]
     end
 
     add_foreign_key :banned_users, :users, column: :user, primary_key: "id"
-    add_foreign_key :banned_users, :events, column: :host, primary_key: "user_id"
+    add_foreign_key :banned_users, :events, column: :host, primary_key: "id"
   end
 end
