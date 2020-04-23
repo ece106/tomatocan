fdfd58a3c4797513a4d51c8bf987fd2eb89ed545
Hudson <hmpace@asu.edu>
2020-04-22 17:06:05 -0700

    Add update user to API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,24 @@ class Api::V1::UsersController < Api::V1::BaseApiController
     def update
         if current_user.nil?
             render :json=> {:success=>false}
-            return
-        end
-        if current_user.name == params[:id]
-            render :json=> {:success=>true}
+        elsif current_user.name == params[:id]
+            if current_user.update_attributes(user_params)
+                render :json=> {:success=>true, :about=>current_user.about}
+            else
+                render :json=> {:success=>false}
+            end
         else
-            render :json=>{:success=>false}
+            render :json=> {:success=>false}
         end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:permalink, :name, :email, :password, 
+                                     :about, :author, :password_confirmation, :genre1, :genre2, :genre3, 
+                                     :twitter, :title, :profilepic, :profilepicurl, :remember_me, 
+                                     :facebook, :address, :latitude, :longitude, :youtube1, :youtube2, 
+                                     :youtube3, :videodesc1, :videodesc2, :videodesc3, :updating_password, 
+                                     :agreeid, :purchid, :bannerpic, :on_password_reset, :stripesignup )
     end
 end
