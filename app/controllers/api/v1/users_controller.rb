f509871f3bb07502fc158ce87d44e0f90c71199c
Hudson <hmpace@asu.edu>
2020-04-24 05:25:53 -0700

    Change format of response for API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::UsersController < Api::V1::BaseApiController
     def update
         if current_user.nil?
-            render :json=> {:success=>false}
+            render :json=> {:success=>false}, :status=>401
+            return
         elsif current_user.name == params[:id]
             if current_user.update_attributes(user_params)
                 render :json=> {:success=>true, :token=>current_user.authentication_token}
+                return
             else
-                render :json=> {:success=>false}
+                render :json=> {:success=>false, :errors=>get_errors}, :status=>422
+                return
             end
         else
-            render :json=> {:success=>false}
+            render :json=> {:success=>false}, :status=>401
+            return
         end
     end
 
