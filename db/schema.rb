5587c8dbe926332aa4635689a62ee28a28baea21
shazzaveri <shahzaibzaveri3@gmail.com>
2020-04-27 20:34:05 -0500

    changed the column type for messages table

--- db/schema.rb
+++ db/schema.rb
@@ -100,16 +100,6 @@ ActiveRecord::Schema.define(version: 201309200000000) do
     t.index ["slug"], name: "index_groups_on_slug", unique: true
   end
 
-  create_table "invites", force: :cascade do |t|
-    t.string "firstname", null: false
-    t.string "lastname", null: false
-    t.string "email", null: false
-    t.integer "phone_number"
-    t.text "description", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "merchandises", id: :serial, force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
@@ -134,7 +124,7 @@ ActiveRecord::Schema.define(version: 201309200000000) do
   create_table "messages", force: :cascade do |t|
     t.string "fullname", null: false
     t.string "email", null: false
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "subject", null: false
     t.text "message", null: false
     t.datetime "created_at", null: false
