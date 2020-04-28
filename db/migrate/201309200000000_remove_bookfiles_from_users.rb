d32ba8d76f3ae843939546691a284ba792200cba
ece106 <lisa@crowdpublish.tv>
2020-04-28 12:35:03 -0700

    Heroku needs to not have improperly named migration

--- db/migrate/201309200000000_remove_bookfiles_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveBookfilesFromUsers < ActiveRecord::Migration[4.2]
-  def change
-    remove_column :users, :bookmobi
-    remove_column :users, :bookepub
-    remove_column :users, :bookkobo
-  end
-end
