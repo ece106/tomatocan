c8aedecd2e95a1a7b30ee4716c5e4e65be1c56c3
honestabej <honestabej@hotmail.com>
2020-04-29 10:02:50 -0700

    facebook code

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable, :omniauth_providers => [:facebook] #:confirmable 
+         
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
