bc2b08d1601ccbd4987827af7ef4a7dc8ee6c47f
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-04-29 02:24:20 -0700

    clean up code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,5 +13,5 @@ Devise.setup do |config|
    config.reset_password_within = 6.hours
    config.sign_out_via = :delete
    config.omniauth_path_prefix = "/users/auth"
-   config.omniauth :facebook, "229746418286819", "d63aa7173d1fd68a75c32801b72f163d", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+   config.omniauth :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, callback_url: "http://thinq.tv/users/auth/facebook/callback"
 end
