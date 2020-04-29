b8b5b6e46d28518f090bc5c2c04280d9945c2372
honestabej <honestabej@hotmail.com>
2020-04-29 10:37:15 -0700

    changed callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,6 @@ Devise.setup do |config|
    config.reset_password_within = 6.hours
    config.sign_out_via = :delete
    config.omniauth_path_prefix = '/users/auth'
-   config.omniauth :facebook, "566814783947187", "841733b04b92defa62fde1b29f0da0ac", callback_url: "https://localhost:3000/users/auth/facebook/callback"
+   config.omniauth :facebook, "566814783947187", "841733b04b92defa62fde1b29f0da0ac", callback_url: "http://localhost:3000/users/auth/facebook/callback"
    #callback_url: "https://boiling-reef-18462.herokuapp.com/users/auth/facebook/callback"
 end
