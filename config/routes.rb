203c89934912d769193cea9127fa01019d093d74
Hudson <hmpace@asu.edu>
2020-04-30 19:34:31 -0700

    change to api routes

--- config/routes.rb
+++ config/routes.rb
@@ -97,7 +97,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
-      resources :users, :only=>[:update]
+      resources :users
       resources :sessions
       resources :events
     end
