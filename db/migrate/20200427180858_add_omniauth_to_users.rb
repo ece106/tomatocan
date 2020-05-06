e0e816cba0b50c501d6a101d902f523fff742df0
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-05-06 11:04:31 -0700

    update migration

--- db/migrate/20200427180858_add_omniauth_to_users.rb
+++ db/migrate/20200427180858_add_omniauth_to_users.rb
@@ -2,6 +2,5 @@ class AddOmniauthToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :image, :text
   end
 end
