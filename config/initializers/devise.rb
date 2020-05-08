b3a444fd30de24e7c1bf43c927e9d28362b05087
ece106 <lisa@crowdpublish.tv>
2020-05-08 09:46:30 -0700

    Devise callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,5 +21,5 @@ Devise.setup do |config|
    config.reset_password_within = 6.hours
    config.sign_out_via = :delete
    config.omniauth_path_prefix = "/users/auth"
-   config.omniauth :facebook, fbappid, secretfacebk, callback_url: "http://localhost:3000/users/auth/facebook/callback"
+   config.omniauth :facebook, fbappid, secretfacebk, callback_url: "https://thinqtv.herokuapp.com/users/auth/facebook/callback"
 end
