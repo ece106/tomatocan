9e72cf7a7e2fc04aa2eb682e4c6e1ec5c66c4069
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-05-12 14:10:07 -0700

    pull

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Crowdpublishtv::Application.config.session_store :active_record_store #:cookie_store, key: '_crowdpublishtv_session'
+Crowdpublishtv::Application.config.session_store :active_record_store, :domain => :all #:cookie_store, key: '_crowdpublishtv_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
