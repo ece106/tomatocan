3b7a6daa61aeddd981cc1d08b12c69d082528e58
ron1502 <ruvelasq@uark.edu>
2020-05-28 14:24:20 -0500

    Presence of name test added

--- test/models/events_test.rb
+++ test/models/events_test.rb
@@ -7,12 +7,23 @@ class EventTest < ActiveSupport::TestCase
 
   end
 
-  test "validates presence of usrid" do
+  test "validate presence of usrid" do
     #usrid present
+    @eventT.usrid = events(:one).usrid  #Ensuring name is present
     assert @eventT.save, "User not saved with present usrid"
 
     #userid absent
     @eventT.usrid = nil
     assert_not @eventT.save, "Event saved with absent usrid"
+  end
+
+  test "validate presence of name" do
+    #name present
+    @eventT.name = events(:one).name  #Ensuring name is present
+    assert @eventT.save, "User not saved with present name"
+
+    #name absent
+    @eventT.name = nil
+    assert_not @eventT.save, "Event saved with absent name"
   end
 end
