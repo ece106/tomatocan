09f8b1ce979c9ef54d976cd57793f1548e29ac13
ron1502 <ruvelasq@uark.edu>
2020-05-27 21:13:26 -0500

    Event model test file created

--- test/models/events_test.rb
+++ test/models/events_test.rb
@@ -2,12 +2,10 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   setup do
-    #Has many test
-    #Usersd
+    #Basic data setup
     @userT = users :one
-    #Events that belong to user :oned
     @eventT = Event.new(events(:one))
     @eventT2 = Event.new(events(:three))
-    #Create instance of event model
+
   end
 end
