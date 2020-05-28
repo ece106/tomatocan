9a4d6f31b982c667cdceb568bf4ec1c666f82939
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-05-28 14:32:00 -0700

    push

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '34.94.16.231', user: 'deploy', roles: %w{app db web}
+server '34.94.48.16', user: 'deploy', roles: %w{app db web}
 
 
 
