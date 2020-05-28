f49fbd06f5bea44b0550ec522a209444086a4fed
ron1502 <ruvelasq@uark.edu>
2020-05-28 14:11:28 -0500

    added presence of usrid test

--- test/models/events_test.rb
+++ test/models/events_test.rb
@@ -3,9 +3,16 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   setup do
     #Basic data setup
-    @userT = users :one
-    @eventT = Event.new(events(:one))
-    @eventT2 = Event.new(events(:three))
+    @eventT = Event.first
 
+  end
+
+  test "validates presence of usrid" do
+    #usrid present
+    assert @eventT.save, "User not saved with present usrid"
+
+    #userid absent
+    @eventT.usrid = nil
+    assert_not @eventT.save, "Event saved with absent usrid"
   end
 end
