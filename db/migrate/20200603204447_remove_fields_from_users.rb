b6bf292f63e0ca9487fd6863632a0fe7cc446573
ron1502 <ruvelasq@uark.edu>
2020-06-04 15:15:39 -0500

    Data type of fields added to allow rollback

--- db/migrate/20200603204447_remove_fields_from_users.rb
+++ db/migrate/20200603204447_remove_fields_from_users.rb
@@ -1,17 +1,17 @@
 class RemoveFieldsFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :ustreamvid
-    remove_column :users, :ustreamsocial
-    remove_column :users, :pinterest
-    remove_column :users, :blogurl
-    remove_column :users, :profilepicurl
-    remove_column :users, :address
-    remove_column :users, :latitude
-    remove_column :users, :longitude
-    remove_column :users, :videodesc1
-    remove_column :users, :videodesc2
-    remove_column :users, :videodesc3
-    remove_column :users, :blogradio
-    remove_column :users, :blogtalkradio
+    remove_column :users, :ustreamvid, :text
+    remove_column :users, :ustreamsocial, :text
+    remove_column :users, :pinterest, :string
+    remove_column :users, :blogurl, :string
+    remove_column :users, :profilepicurl, :string
+    remove_column :users, :address, :string
+    remove_column :users, :latitude, :float
+    remove_column :users, :longitude, :float
+    remove_column :users, :videodesc1, :string
+    remove_column :users, :videodesc2, :string
+    remove_column :users, :videodesc3, :string
+    remove_column :users, :blogradio, :string
+    remove_column :users, :blogtalkradio, :text
   end
 end
