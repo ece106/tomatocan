84927f612d5451109d43fd5ca322c7d30829f11a
ron1502 <ruvelasq@uark.edu>
2020-06-08 16:51:23 -0500

    Commented out function rewrite

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -24,7 +24,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # The path used after confirmation.
-  def after_confirmation_path_for(resource_name, resource)
-     super(resource_name, resource)
-  end
+  # def after_confirmation_path_for(resource_name, resource)
+  #   super(resource_name, resource)
+  # end
 end
