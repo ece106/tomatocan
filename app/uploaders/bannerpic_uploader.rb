d140b63a670e30ba93af52a676938fbee562f3aa
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-06-09 13:54:16 -0700

    setup s3

--- app/uploaders/bannerpic_uploader.rb
+++ app/uploaders/bannerpic_uploader.rb
@@ -7,7 +7,7 @@ class BannerpicUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   if Rails.env.development? || Rails.env.test?
-    storage :file  # but what if I want to test fog/aws
+    storage :fog  # but what if I want to test fog/aws
   else
     storage :fog
   end
