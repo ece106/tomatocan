3860cee3debc42110a87ec5c7017d84a5bc14250
blake <brobs0111@gmail.com>
2020-06-16 14:16:13 -0500

    guest_visits_home_page_test.rb and user_edits_profile.rb updated and working correctly

--- test/integration/users/control_panel/user_edits_profile.rb
+++ test/integration/users/control_panel/user_edits_profile.rb
@@ -1,5 +1,5 @@
-require "test_helper"
-require "capybara-screenshot/minitest"
+require 'test_helper'
+require 'capybara-screenshot/minitest'
 
 class UserEditsProfile < ActionDispatch::IntegrationTest
   setup do
@@ -12,7 +12,7 @@ class UserEditsProfile < ActionDispatch::IntegrationTest
 
   test "can edit profile page with correct attributes" do
     fill_in id: "user_name", with: "Cody Karunas"
-    fill_in id: "user_about", with: "About me description"
+    fill_in id: "user_about", with: "About you"
     fill_in id: "user_genre1", with: "Test Category"
     fill_in id: "user_genre2", with: "Test Category"
     fill_in id: "user_genre3", with: "Test Category"
@@ -22,15 +22,6 @@ class UserEditsProfile < ActionDispatch::IntegrationTest
     assert page.has_content? "Cody Karunas"
     assert page.has_content? "Test Category"
     assert page.has_content? "Test Category"
-  end
-
-  test "can edit social media with correct attributes" do
-    fill_in id: "user_twitter", with: "test_twitter_handle"
-    fill_in id: "user_youtube1", with: "http://www.youtube.com/watch?v=/frlviTJcVUo"
-    fill_in id: "user_youtube2", with: "http://www.youtube.com/watch?v=/frlviTJcVUo"
-    fill_in id: "user_youtube3", with: "http://www.youtube.com/watch?v=/frlviTJcVUo"
-
-    find(id: "cancelProfileButton", match: :first).click
   end
 
   test "can cancel edit profile page" do
