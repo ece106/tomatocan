59958a334afa25f570bf09ceef3ae0fe561deb81
vinhnguyen200891337 <vinhnguyen200891337@gmail.com>
2020-06-17 13:12:13 -0700

    Push test

---
+++ config/initializers/aakeys.rb
@@ -0,0 +1,11 @@
+DEVISE_SECRET_KEY = 'fake'
+AWS_KEY = 'morefake'
+AWS_SECRET_KEY = 'pretend'
+AWS_BUCKET = 'yourawsbucketname'
+STRIPE_SECRET_KEY = "madeup"
+STRIPE_PUBLIC_KEY = "allfake"
+GMAIL_PWD = "superfake"
+Stripe.api_key = STRIPE_SECRET_KEY
+STRIPE_CONNECT_CLIENT_ID = "superfake"
+FACEBOOK_APP_ID = "numbers"
+FACEBOOK_APP_SECRET = "secret"
