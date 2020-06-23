ed0b2e3deb0c724dadbf9fd2a6d2a405cfb47ea3
ron1502 <ruvelasq@uark.edu>
2020-06-23 15:30:54 -0500

    Already registered users are confirmed

--- db/migrate/20200603200725_add_user_devise_confirmation.rb
+++ db/migrate/20200603200725_add_user_devise_confirmation.rb
@@ -4,5 +4,12 @@ class AddUserDeviseConfirmation < ActiveRecord::Migration[6.0]
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
+
+    say "CONFIRMING ALREADY REGISTERED USERS"
+    User.find_each do |user|
+      user.confirmation_sent_at = Time.now - 10.minutes
+      user.confirmed_at = Time.now
+      user.save
+    end
   end
 end
