60967fb58c0d66604ea48a679857e765aaa9f325
Suvanch <Suvanch@gmail.com>
2020-06-25 09:14:24 -0500

    Finished merging with master

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,24 +11,25 @@ class StaticPagesController < ApplicationController
 
     @events = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", showrecentconvo, 'Study Hall', 'User Research' ).order('start_at ASC').paginate(page: params[:page], :per_page => 9)
     @eventsAll = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", showrecentconvo, 'Study Hall', 'User Research' )
+    @monthforCalendar = Date.today
     pdtnow = Time.now - 7.hours
     pdtnext = Time.now - 8.hours
     currconvo = Event.where( "start_at < ? AND start_at > ? AND topic = ?", pdtnow, pdtnext, 'Conversation' ).first
-    nextconvo = Event.where( "start_at > ? AND topic = ?", pdtnow, 'Conversation' ).order('start_at ASC').first 
+    nextconvo = Event.where( "start_at > ? AND topic = ?", pdtnow, 'Conversation' ).order('start_at ASC').first
 
     currstudy = Event.where( "start_at < ? AND start_at > ? AND (topic = ? OR topic = ?)", pdtnow, pdtnext, 'Study Hall', 'User Research' ).order('start_at ASC').first
-    nextstudy = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", pdtnow, 'User Research', 'Study Hall' ).order('start_at ASC').first 
+    nextstudy = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", pdtnow, 'User Research', 'Study Hall' ).order('start_at ASC').first
 
     if currconvo.present?
       @displayconvo = currconvo
-    else 
+    else
       @displayconvo = nextconvo
-    end  
+    end
     if currstudy.present?
       @displaystudy = currstudy
-    else 
+    else
       @displaystudy = nextstudy
-    end  
+    end
 
     if @displayconvo.present?
       @name = @displayconvo.name
@@ -36,7 +37,7 @@ class StaticPagesController < ApplicationController
       @start_time = @displayconvo.start_at.strftime("%B %d %Y") + ' ' + @displayconvo.start_at.strftime("%T") + " PDT"
       @end_time = @displayconvo.end_at.strftime("%B %d %Y") + ' ' + @displayconvo.end_at.strftime("%T") + " PDT"
       @host = User.find(@displayconvo.usrid)
-    end  
+    end
     if @displaystudy.present?
       @namestudy = @displaystudy.name
       @descriptionstudy = @displaystudy.desc
@@ -44,8 +45,8 @@ class StaticPagesController < ApplicationController
       @end_timestudy = @displaystudy.end_at.strftime("%B %d %Y") + ' ' + @displaystudy.end_at.strftime("%T") + " PDT"
       @hoststudy = User.find(@displaystudy.usrid)
       @topic = @displaystudy.topic
-    end  
-        
+    end
+
     if user_signed_in?
       @user = User.find(current_user.id)
     end
@@ -60,13 +61,13 @@ class StaticPagesController < ApplicationController
     pdtnext = Time.now - 8.hours
 
     currstudy = Event.where( "start_at < ? AND start_at > ? AND (topic = ? OR topic = ?)", pdtnow, pdtnext, 'Study Hall', 'User Research' ).order('start_at ASC').first
-    nextstudy = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", pdtnow, 'User Research', 'Study Hall' ).order('start_at ASC').first 
+    nextstudy = Event.where( "start_at > ? AND (topic = ? OR topic = ?)", pdtnow, 'User Research', 'Study Hall' ).order('start_at ASC').first
 
     if currstudy.present?
       @displaystudy = currstudy
-    else 
+    else
       @displaystudy = nextstudy
-    end  
+    end
 
     if @displaystudy.present?
       @namestudy = @displaystudy.name
@@ -74,8 +75,8 @@ class StaticPagesController < ApplicationController
       @start_timestudy = @displaystudy.start_at.strftime("%B %d %Y") + ' ' + @displaystudy.start_at.strftime("%T") + " PDT"
       @end_timestudy = @displaystudy.end_at.strftime("%B %d %Y") + ' ' + @displaystudy.end_at.strftime("%T") + " PDT"
       @hoststudy = User.find(@displaystudy.usrid)
-    end  
-        
+    end
+
     if user_signed_in?
       @user = User.find(current_user.id)
     end
