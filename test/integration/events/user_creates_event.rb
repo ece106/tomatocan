90422ba9db677b08c3a6f330c78e9472b028635c
RogelioMart <rogelio.mart@hotmail.com>
2020-07-03 16:07:55 -0700

    Finished making the tests work.

--- test/integration/events/user_creates_event.rb
+++ test/integration/events/user_creates_event.rb
@@ -7,17 +7,14 @@ class UserCreatesEvent < ActionDispatch::IntegrationTest
 
     user_sign_in @test_user
 
-    visit edit_event_path @event
+    visit new_event_path
   end
 
 =begin
-As of 07/01/2020 all tests work
+As of 07/02/2020 all tests work
 
 READ ME
 These tests focus on creating valid and invalid events
-
-Note: options start at 1 so for the month option[1] = January, for day option[5] = day 5, and 
-so on.
 
 Capybara doesn't automatically set hidden fields.
 For tests that use hidden fields you should set them manually.
@@ -30,12 +27,11 @@ time_zone_test = "-07:00"
     fill_in id:'event_name', with: 'Valid Event Name'
     fill_in id: 'event_desc', with: 'Valid Event Description'
 	
-	find('#event_topic').find(:xpath, 'option[2]').select_option #option[1]= study hall, option[2]= conversation, option[3]=  User research
+	find('#event_topic').find(:xpath, 'option[2]').select_option #topic
 	
 	#NOTE when new years are added and old ones deleted these tests might error or fail.
-	#Date selected for test December 31, 2021 at 1:00pm
-	find('#event_start_at_1i').find(:xpath, 'option[2]').select_option # selects year option[1] = 2020, option[2] = 2021.
-	
+	#Date selected for test July 2, 2020
+	find('#event_start_at_1i').find(:xpath, 'option[2]').select_option # Selects year
 	find('#event_start_at_2i').find(:xpath, 'option[12]').select_option # selects month
 	find('#event_start_at_3i').find(:xpath, 'option[31]').select_option # selects day 
 	find('#event_start_at_4i').find(:xpath, 'option[14]').select_option # selects hour
@@ -44,10 +40,13 @@ time_zone_test = "-07:00"
 	find(:xpath, "//*[@id='timeZone']", visible: false).set time_zone_test #sets the timezone manually
 
     click_on id: 'eventSubmit', match: :first
-
+	
+	#"calendar-time-day2"
     assert_equal current_path, root_path
     assert page.has_content? "Valid Event Name"
 	assert page.has_content? "userconfirmed"
+	assert page.has_content? "July 2"
+	
   end
   
   test "create event with empty string name" do
@@ -55,7 +54,6 @@ time_zone_test = "-07:00"
     fill_in id: "event_desc", with: ""
 	
 	find('#event_topic').find(:xpath, 'option[2]').select_option #selects topic
-	#Date selected for test July 1, 2021 12:00am Thursday
 	find('#event_start_at_1i').find(:xpath, 'option[2]').select_option#Selects year
 	find('#event_start_at_2i').find(:xpath, 'option[7]').select_option#select month
 	find('#event_start_at_3i').find(:xpath, 'option[1]').select_option#selects day
@@ -66,8 +64,8 @@ time_zone_test = "-07:00"
 
 	click_on id: 'eventSubmit', match: :first
 	
-	#Since there are empty strings through out the page it is better to see if there is a date with this associated with the event that was attempted to be created.
-	assert_not page.has_content? "Thu, July 1"
+	assert page.has_css? "#error_explanation"
+	assert page.has_content? "Name can't be blank"
 	
   end
 
@@ -76,7 +74,6 @@ time_zone_test = "-07:00"
     fill_in id: "event_desc", with: "Blank spaces"
 	
 	find('#event_topic').find(:xpath, 'option[2]').select_option #selects topic
-	#Date selected for test July 3, 2021 12:00am Saturday
 	find('#event_start_at_1i').find(:xpath, 'option[2]').select_option #Selects year
 	find('#event_start_at_2i').find(:xpath, 'option[7]').select_option #select month
 	find('#event_start_at_3i').find(:xpath, 'option[1]').select_option #selects day
@@ -87,17 +84,16 @@ time_zone_test = "-07:00"
 
 	click_on id: 'eventSubmit', match: :first
 	
-	#Since there are empty strings through out the page it is better to see if there is a date with this associated with the event that was attempted to be created.
-	assert_not page.has_content? "Sat, July 3"
+	assert page.has_css? "#error_explanation"
+	assert page.has_content? "Name can't be blank"
 	
   end
 
-  test "create event with URL desc" do
+  test "create event with invalid URL desc" do
     fill_in id: "event_name", with: "Not Valid Desc"
     fill_in id: "event_desc", with: "thinq.tv"
 	
 	find('#event_topic').find(:xpath, 'option[2]').select_option #selects topic
-	#Date selected for test July 3, 2021 1:00am Saturday
 	find('#event_start_at_1i').find(:xpath, 'option[2]').select_option #Selects year
 	find('#event_start_at_2i').find(:xpath, 'option[7]').select_option #select month
 	find('#event_start_at_3i').find(:xpath, 'option[1]').select_option #selects day
@@ -108,7 +104,8 @@ time_zone_test = "-07:00"
 
     click_on class: "btn btn-lg btn-primary", match: :first
 
-	assert_not page.has_content? "Not Valid Desc"
+	assert page.has_css? "#error_explanation"
+	assert page.has_content? "Desc riptions ...URLs are not allowed in event descriptions. Keep in mind that people will be searching here for actual gatherings that they can attend, or to find out when you'll be livestreaming. They will not be searching for sites to browse. Paste all information attendees need here."
 	
   end
 
