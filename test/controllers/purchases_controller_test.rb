1cd7f1256d7e929500debffca0ab18912b1f1ea3
ron1502 <ruvelasq@uark.edu>
2020-07-09 16:48:07 -0500

    Tests for purchase controller finished

--- test/controllers/purchases_controller_test.rb
+++ test/controllers/purchases_controller_test.rb
@@ -5,7 +5,7 @@ require 'stripe'
 
 class PurchasesControllerTest < ActionController::TestCase
 
-	 include ActiveJob::TestHelper
+  include ActionMailer::TestHelper
 
   setup do
     @purchases = purchases(:one)
@@ -14,15 +14,16 @@ class PurchasesControllerTest < ActionController::TestCase
     @token = Stripe::Token.create(card: { number: '4242424242424242',
                                           exp_month: 8, exp_year: 2050,
                                           cvc: 132})
-    @purchase_info ={ user_id: @purchaser.id,
-                      author_id: users(:one).id,
-                      stripe_customer_token: @purchaser.stripe_customer_token,
-                      stripe_card_token: @token.id,
-                      pricesold: 10 }
+    @purchases.stripe_customer_token = @purchaser.stripe_customer_token
+    @purchases.stripe_card_token = @token.id
+
     @customer = Stripe::Customer.create(description: @purchaser.name,
                                         email: @purchaser.email)
     @merchandise = merchandises(:one)
     @donation_merchandise = merchandises(:seven)
+
+    @default_donation = purchases(:donation_no_merchandise)
+    @default_donation.stripe_card_token = @token.id
   end
 
   test 'should_get_purchases_new_purchase' do
@@ -54,24 +55,31 @@ class PurchasesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'purchase with merchandise sends mail' do
-    @purchase_info[:merchandise_id] = merchandises(:one).id
-    post :create, params: { purchase: @purchase_info }
-    assert_enqueued_jobs(2)
+  test 'Default donation' do
+    seller = User.find(@default_donation.author_id)
+    post :create, params: { purchase: @default_donation.attributes}
+    assert_enqueued_emails 2
+    assert_equal "You successfully donated $#{@default_donation.pricesold} . Thank you for being a donor of #{seller.name}",
+      flash[:notice]
+    assert_redirected_to user_profile_path(seller.permalink)
+  end
+
+  test 'Merchandise purchase' do
+    @purchases.merchandise_id = merchandises(:one).id
+    post :create, params: { purchase: @purchases.attributes }
+    assert_enqueued_emails 2
     assert_equal "You have successfully completed the purchase! Thank you for being a patron of #{@seller.name}",
       flash[:success]
     assert_redirected_to user_profile_path(@seller.permalink)
-    # There should be 2 emails in the box one for each seller one for buyer
   end
 
-  test 'successful donation' do
+  test 'Merchandise donation' do
     sign_in @purchaser
-    @purchase_info[:merchandise_id] = @donation_merchandise.id
-    post :create, params: { purchase: @purchase_info }
-    assert_enqueued_jobs 2
-    assert_equal "You successfully donated $#{@donation_merchandise.price.to_s} . Thank you for being a donor of #{@seller.name}",
+    @purchases.merchandise_id = @donation_merchandise.id
+    post :create, params: { purchase: @purchases.attributes }
+    assert_enqueued_emails 2
+    assert_equal "You successfully donated $#{@donation_merchandise.price} . Thank you for being a donor of #{@seller.name}",
       flash[:notice]
     assert_redirected_to user_profile_path(@seller.permalink)
   end
-
 end
