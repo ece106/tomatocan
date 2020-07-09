5141ce00ce7453fb95ecf803760bd009ef736380
Hieu Phan <phanh@miamioh.edu>
2020-07-09 15:04:06 -0400

    made changes based on Ronald feedback

--- test/integration/navbar/nonuser_navbar_test.rb
+++ test/integration/navbar/nonuser_navbar_test.rb
@@ -40,6 +40,11 @@ class NonuserNavbar < ActionDispatch::IntegrationTest
     assert page.has_link? 'Sign Up'
     find_link('Sign Up', match: :first).click
     assert_equal '/signup', current_path
+    assert_emails 2 do
+        #Confirmation email is sent after signup
+      user_sign_up @user
+    end
+    assert_equal '/login', current_path
   end
 
   test 'sign in' do
