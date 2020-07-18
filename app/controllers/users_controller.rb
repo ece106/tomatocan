5a928ee5c2fda309c8a622919b041c135517c793
blake <brobs0111@gmail.com>
2020-07-18 12:38:11 -0500

    Messing with partial. Showing past RSVP'd events.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     @calendar_events = @calendar_events.paginate(page: params[:page], :per_page => 4)
     @currentRSVP = rsvps.flat_map{ |t| t.calendar_events(t.start_at)}
     @currentRSVP = @currentRSVP.sort_by{|event| event.start_at}
-    @currentRSVP = @currentRSVP.paginate(page: params[:page], :per_page => 4)
+    @currentRSVP = @currentRSVP.paginate(page: params[:page], :per_page => 8)
 
     respond_to do |format|
       format.html  #show.html.erb
