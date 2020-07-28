99e99a40ac669b807b61f705c2f75c267128278f
Anderson Sanchez <andersonsanchez53@gmail.com>
2020-07-28 14:18:09 -0700

    stats feat

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,10 @@
+class StatsController < ApplicationController
+    def upload_stats
+        current_user = User.find_by_id(params[:user])
+        current_user.update({'stats': current_user.stats << params[:stats]})
+    end
+
+    def get_stats
+
+    end
+end
