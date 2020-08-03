3b26c0d4a8cdc7a9ad6dc47752a51c8c9079b9d0
Laura <laura@reboot.moe>
2020-08-03 13:52:14 -0600

    Wrote tests for block and unblock

--- test/controllers/block_controller_test.rb
+++ test/controllers/block_controller_test.rb
@@ -3,26 +3,61 @@ class BlockControllerTest < ActionController::TestCase
     include ActiveJob::TestHelper
 
   setup do 
-    @user = users(:one)
-    @blocked_user = users(:two)
-    @user_to_unblock = users(:three)
+    @user = users :one
+    @blocked_user = users :two
+    post :block, params: {to_block: @blocked_user.id, owner: @user.id}
   end
 
   #block
+  test 'blockedBy_not_empty' do
+    #post :block, params: {to_block: @blocked_user.id, owner: @user.id}
+    assert_not_empty User.find_by_id(@blocked_user.id).blockedBy
+  end
+
+  test 'block_response_ok' do
+    assert_response :ok
+  end
+
   test 'convo_owner_added_to_blockedBy_array' do
-    
+    blockedBy = User.find_by_id(@blocked_user.id).blockedBy
+    assert_includes blockedBy, @user.permalink
   end
       
   test 'blocked_user_added_to_BlockedUsers_array' do
-
+    blockedUsers = User.find_by_id(@user.id).BlockedUsers
+    assert_includes blockedUsers, @blocked_user.permalink
   end
 
-  #test 'already_blocked_user_not_reblocked'
+  test 'already_blocked_user_not_reblocked' do
+    post :block, params: {to_block: @blocked_user.id, owner: @user.id}
+    blockedUsers = User.find_by_id(@user.id).BlockedUsers
+    assert_not_equal blockedUsers[1], @blocked_user.permalink
+    assert_equal blockedUsers.count, 1
+  end
+
+  test 'BlockedUsers_array_has_correct_count_after_block' do
+     blockedUsers = User.find_by_id(@user.id).BlockedUsers
+     assert_equal blockedUsers.count, 1
+  end
 
   #unblock
-  #test 'unblocked_user_removed_from_BlockedUsers_array'
+  test 'unblocked_user_removed_from_BlockedUsers_array' do
+    blockedUsers = User.find_by_id(@user.id).BlockedUsers
+    assert_includes blockedUsers, @blocked_user.permalink
+    
+    post :unblock, params: {to_unblock_id: @blocked_user.id, current_user_id: @user.id}
+    blockedUsers = User.find_by_id(@user.id).BlockedUsers
+    assert_not_includes blockedUsers, @blocked_user.permalink
+  end
 
-  #test 'convo_owner_removed_from_blockedBy_array'
+  test 'convo_owner_removed_from_blockedBy_array' do
+    blockedBy = User.find_by_id(@blocked_user.id).blockedBy
+    assert_includes blockedBy, @user.permalink
+    
+    post :unblock, params: {to_unblock_id: @blocked_user.id, current_user_id: @user.id}
+    blockedBy = User.find_by_id(@blocked_user.id).blockedBy
+    assert_not_includes blockedBy, @user.permalink
+  end
 
   #unload
   
@@ -34,4 +69,4 @@ class BlockControllerTest < ActionController::TestCase
 
 
   #signedin?
-
+end
