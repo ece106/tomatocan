146918a0a1b1d6a0ca7cac472fe6a329b4f5b6ef
Tawataka <treynavi@ucsc.edu>
2020-08-17 14:55:35 -0700

    no head

---
+++ config/environments/development.rb
@@ -0,0 +1,38 @@
+Rails.application.configure do
+
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :user_name => "thinqtesting@gmail.com",
+  :password => 'thinqtesting123',
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+}
+config.action_mailer.default_url_options = {
+  host: "localhost:3000", protocol: "http"
+}
+  config.cache_classes = false
+  config.eager_load = false
+  config.consider_all_requests_local = true
+
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
+  end
+
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+  config.assets.quiet = true
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+end
---
+++ config/initializers/aakeys.rb
@@ -0,0 +1,13 @@
+DEVISE_SECRET_KEY = 'fake'
+AWS_KEY = 'morefake'
+AWS_SECRET_KEY = 'pretend'
+AWS_BUCKET = 'yourawsbucketname'
+STRIPE_SECRET_KEY = "madeup"
+STRIPE_PUBLIC_KEY = "allfake"
+GMAIL_PWD = "superfake"
+Stripe.api_key = STRIPE_SECRET_KEY
+STRIPE_CONNECT_CLIENT_ID = "superfake"
+FACEBOOK_APP_ID = "numbers"
+FACEBOOK_APP_SECRET = "secret"
+GOOGLE_CLIENT_SECRET = 'fake'
+GOOGLE_CLIENT_ID = 'fake'
