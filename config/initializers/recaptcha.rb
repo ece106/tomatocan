cc25af3500fa60b33cab83151cd78b57b4d3c4cb
Hieu Phan <phanh@miamioh.edu>
2020-08-21 18:01:52 -0400

    fixed error

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.site_key  = RECAPTCHA_PUBLIC_KEY
+  config.secret_key = RECAPTCHA_PRIVATE_KEY
 end
