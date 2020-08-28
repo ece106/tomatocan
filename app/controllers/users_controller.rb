d9bcacd2ff18a94f48c55ff319358dad415d59e7
ece106 <lisa@crowdpublish.tv>
2020-08-27 21:43:52 -0700

    Captcha and rewording

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -165,13 +165,18 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    @recaptcha_checked = verify_recaptcha(model: @user)
 
-    if @user.save
+    if @recaptcha_checked 
+      if @user.save
       redirect_to new_user_session_path, success: "You have successfully signed up! An email has been sent for you to confirm your account."
       UserMailer.with(user: @user).welcome_email.deliver_later
-    else
-       redirect_to new_user_signup_path, danger: signup_error_message
-      #redirect_to new_user_signup_path
+      else
+        redirect_to new_user_signup_path, danger: signup_error_message
+        @user.errors.clear
+      end
+    else 
+      redirect_to new_user_signup_path, danger: signup_error_message + "Please check the captcha box!"
       @user.errors.clear
     end
   end
