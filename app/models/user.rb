604e23bb0d46a18e74fd0a32782628f4fcba3c4f
KaRob121 <karlrobin_espinosa@yahoo.com>
2020-09-13 20:28:44 -0700

    Updated User and Notification models

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :rsvpqs
   has_many :events, :through => :rsvpqs
   has_many :merchandises
+  has_many :notifications, foreign_key: :recipient_id
 
   # Active Relationships (A user following a user)
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id" #, dependent: :destroy (if a user is deleted, delete the relationship)
