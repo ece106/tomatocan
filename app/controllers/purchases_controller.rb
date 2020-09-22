75954a1cc2d4556a970fedaf9244077556ba59c5
Laura <laura@reboot.moe>
2020-09-22 14:01:45 -0600

    Working on fixing redirects when purchasing with attachments

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -75,9 +75,16 @@ class PurchasesController < ApplicationController
           filename_and_data = @merchandise.get_filename_and_data
           filename = filename_and_data[:filename]
           data = filename_and_data[:data]
-          send_data_to_buyer data, filename and return
-          redirect_to user_profile_path(@seller.permalink) 
-          flash[:success] = "You have successfully completed the purchase! Thank you for being a patron of " + @seller.name
+          #send_data_to_buyer data, filename
+          redirect_later data.url, "Downloading file"
+          respond_to do |format|
+            format.html { redirect_to user_profile_path(@seller.permalink) }
+            format.pdf { send_data_to_buyer data, filename and return }
+          end
+          #redirect_later user_profile_path(@seller.permalink), "Downloading file"
+          #send_data_to_buyer data, filename and return
+          #redirect_to user_profile_path(@seller.permalink) 
+          #flash[:success] = "You have successfully completed the purchase! Thank you for being a patron of " + @seller.name
           PurchaseMailer.with(@purchase_mailer_hash).purchase_saved.deliver_later
           PurchaseMailer.with(@purchase_mailer_hash).purchase_received.deliver_later
         when false
