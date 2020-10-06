95f524bc3d3d91c835f498b33d60aaa9268e55e0
Tawataka <treynavi@ucsc.edu>
2020-10-06 11:43:31 -0700

    most recent version

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
+
   attributes :name, :permalink, :profilepic, :about, :genre1, :genre2, :genre3, :bannerpic, :resume
-=======
-  attributes :name, :permalink, :profilepic, :about, :genre1, :genre2, :genre3, :bannerpic
->>>>>>> fa1cbed1d9dbd78d6c41da7c4c8ccad647828185
+
 end
