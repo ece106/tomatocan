1977382c3a2f0fcc77a4a9bc873a45a64be82e3c
Nick <jlord-ender-laing17@brophybroncos.org>
2020-11-07 14:05:11 -0700

    Fleshed out controller for creation of embed link

--- app/controllers/embed_links_controller.rb
+++ app/controllers/embed_links_controller.rb
@@ -1,6 +1,14 @@
 class EmbedLinksController < ApplicationController
   before_action :set_embed_link, only: [:show, :edit, :update, :destroy]
 
+  @@tempBorder = false
+  @@tempBorderColor = " "
+  @@tempWidth = " "
+  @@tempHeight = " "
+  @@tempBottom = 0
+  @@tempRight = 0
+  @@tempPosition = " "
+
   # GET /embed_links
   def index
     @embed_links = EmbedLink.all
@@ -17,6 +25,7 @@ class EmbedLinksController < ApplicationController
 
   # GET /embed_links/1/edit
   def edit
+    
   end
 
   # POST /embed_links
@@ -24,7 +33,63 @@ class EmbedLinksController < ApplicationController
     @embed_link = EmbedLink.new(embed_link_params)
 
     if @embed_link.save
-      redirect_to @embed_link, notice: 'Embed link was successfully created.'
+
+      newHeight = ""
+      newWidth = ""
+      case embed_link_params["size"]
+      when "A small portion of the screen"
+        newHeight = "200px"
+        newWidth = "400px"
+      when "A decent portion of the screen"
+        newHeight = "400px"
+        newWidth = "800px"
+      when "A large portion of the screen"
+        newHeight = "800px"
+        newWidth = "1000px"
+      else
+        newHeight = "400px"
+        newWidth = "800px"
+      end
+
+      @@tempHeight = newHeight
+      @@tempWidth = newWidth
+
+      @@tempBorder = embed_link_params["border"]
+      @@tempBorderColor = embed_link_params["border_color"]
+
+      newBottom = 0
+      newRight = 1
+      case embed_link_params["location"]
+      when "The lower left corner"
+        newBottom = 0
+        newRight = 1
+      when "The upper left corner"
+        newBottom = 1
+        newRight = 1
+      when "The upper right corner"
+        newBottom = 1
+        newRight = 0
+      when "The lower right corner"
+        newBottom = 0
+        newRight = 0
+      else
+        newBottom = -1
+        newRight = -1
+      end
+      @@tempBottom = newBottom
+      @@tempRight = newRight
+
+      newSpecial = "";
+      case embed_link_params["special_position"]
+      when "I want users to be able to scroll past the embed page"
+        newSpecial = "absolute"
+      else
+        newSpecial = "fixed"
+      end
+      @@tempPosition = newSpecial  
+
+      # redirect_to @embed_link, notice: 'Embed link was successfully created.'
+      redirect_to new_embed_link_confirm_path, success: embed_error_message + "Your code has been crafted!"
     else
       render :new
     end
@@ -43,6 +108,11 @@ class EmbedLinksController < ApplicationController
   def destroy
     @embed_link.destroy
     redirect_to embed_links_url, notice: 'Embed link was successfully destroyed.'
+  end
+
+  def embed_error_message
+    msg = ""
+    return msg
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -43,10 +43,10 @@ Rails.application.routes.draw do
   match "invites/1/edit", to: 'invites#edit', via: 'get'
   get "invites/1/edit", :to => 'invites#edit', :as => :new_invite_confirm
 
-  match 'embed_help', to: 'embed_links#new', via: 'get'
-  get "embed_help", :to => 'embed_links#new', :as => :new_embed_link_form
-  match 'embed_help/1/confirm', :to => 'embed_links#edit', via: 'get'
-  get 'embed_help/1/confirm', :to => 'embed_links#edit', :as => :new_embed_link_confirm
+  match 'embed_link', to: 'embed_links#new', via: 'get'
+  get "embed_link", :to => 'embed_links#new', :as => :new_embed_link_form
+  match 'embed_links/1/edit', :to => 'embed_links#edit', via: 'get'
+  get 'embed_links/1/edit', :to => 'embed_links#edit', :as => :new_embed_link_confirm
 
   as :user do
   match 'signup/:refer_id', to: 'devise/registrations#new', via: 'get'
