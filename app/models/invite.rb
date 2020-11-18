9eeedec30dac88ec92facafbdb82a0ed5810482e
Nick <jlord-ender-laing17@brophybroncos.org>
2020-11-17 17:54:42 -0700

    Fixed invites db issue

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,21 @@
-class Invite < ApplicationRecord
-end
+class Invite
+
+    include ActiveModel::Validations
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+  
+    attr_accessor :phone_number, :country_code, :relationship, :preferred_name, :sender_id
+  
+    validates :phone_number, :presence => true
+  
+    def initialize(attributes = {})
+      attributes.each do |name, value|
+        send("#{name}=", value)
+      end
+    end
+  
+    def persisted?
+      false
+    end
+  
+  end
