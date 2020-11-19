8a6fbb056cbca96864464c0d1eac4655ea8ae401
gaknight <gacknight@gmail.com>
2020-11-18 22:10:58 -0700

    Fix white space

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -8,11 +8,13 @@ class InvitesController < ApplicationController
     @invite = Invite.new
   end
 
+  # GET /invite/:referer_id
   def invite_received
     # create cookie
     cookies[:referer_id] = {
-    value: params[:referer_id],
-    expires: 1.month.from_now}
+      value: params[:referer_id],
+      expires: 1.month.from_now
+    }
     # trigger redirect
     redirect_to "/"
     end
@@ -26,24 +28,23 @@ class InvitesController < ApplicationController
 
   # POST /invites
   def create
-      if current_user == nil
-        redirect_to new_invite_form_path, danger: invite_error_message + "You must be signed in!"
-      else
+    if current_user == nil
+      redirect_to new_invite_form_path, danger: invite_error_message + "You must be signed in!"
+    else
 
       invite_params["sender_id"] = current_user.id
 
-  
-        if verify_recaptcha(model: @invite)
-  
-          countryAb = invite_params["country_code"]
-          countryCode = IsoCountryCodes.find(countryAb).calling
-          phoneNum = invite_params["phone_number"]
-          completeNum = countryCode + phoneNum
-          @@globalNum = completeNum
+      if verify_recaptcha(model: @invite)
 
-          messageBody = ""
+        countryAb = invite_params["country_code"]
+        countryCode = IsoCountryCodes.find(countryAb).calling
+        phoneNum = invite_params["phone_number"]
+        completeNum = countryCode + phoneNum
+        @@globalNum = completeNum
 
-          case invite_params["relationship"]
+        messageBody = ""
+
+        case invite_params["relationship"]
           when "Friends"
             messageBody = "ThinQ.tv Invite from " + current_user.name.titleize + "!: %0D%0AHey, " + invite_params["preferred_name"] + ", come check out%0D%0A %0D%0Ahttps://thinq.tv/invite/" + current_user.id.to_s + "%0D%0A%0D%0Aand get tips from industry pros!"
           when "Family"
@@ -54,17 +55,17 @@ class InvitesController < ApplicationController
             messageBody = "ThinQ.tv Invite from " + current_user.name.titleize + ": %0D%0AHi " + invite_params["preferred_name"] + ", " + current_user.name.titleize + " has invited you to join ThinQ. Sign up at %0D%0A%0D%0Ahttps://thinq.tv/invite/" + current_user.id.to_s + " %0D%0A%0D%0Ato get tips from industry pros, and share your own knowledge in hosted thoughtful conversations!"
           else
             messageBody = "ThinQ.tv Invite from " + current_user.name.titleize + ": %0D%0AHi " + invite_params["preferred_name"] + ", " + current_user.name.titleize + " has invited you to join ThinQ. Sign up at %0D%0A%0D%0Ahttps://thinq.tv/invite/" + current_user.id.to_s + " %0D%0A%0D%0Ato get tips from industry pros, and share your own knowledge in hosted thoughtful conversations!"
-          end
-
-          @@globalMessage = messageBody
-
-          redirect_to new_invite_confirm_path, success: invite_error_message + "Your invite has been crafted!"
-  
-        else
-          redirect_to new_invite_form_path, danger: invite_error_message + "Please check the captcha box!"
         end
+
+        @@globalMessage = messageBody
+
+        redirect_to new_invite_confirm_path, success: invite_error_message + "Your invite has been crafted!"
+  
+      else
+        redirect_to new_invite_form_path, danger: invite_error_message + "Please check the captcha box!"
       end
     end
+  end
 
   def invite_error_message
     msg = ""
