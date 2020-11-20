48be60b09674cec23bd50024a435cec29de5b532
KaRob121 <karlrobin_espinosa@yahoo.com>
2020-11-20 14:54:43 -0700

    Made the home page calendar view responsive

---
+++ app/assets/javascripts/monthlyCalendar.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $('.days').click(function() { 
+        if ($( window ).width() < 750) {
+            // Get the modal for the current day
+            var currModal = $(this).find("div[id*='modal']")[0]
+
+            // Get the <span> element that closes the modal
+            var closeButton = $(this).find("span[class*='close']")[0]
+            console.log(closeButton)
+
+            // Show the modal
+            $(currModal).show();
+
+            // When the user clicks anywhere outside of the modal or on the x button, close it
+            window.onclick = function(event) {
+                if (event.target == currModal || event.target == closeButton) {
+                    $(currModal).hide();
+                }
+            }
+
+            // When the user resizes the window to a larger size and the modal was showing, close the modal
+            $( window ).resize(function() {
+                console.log($(currModal).attr("display"))
+                if ($(currModal).attr("display") != "none") {
+                    $(currModal).hide();
+                }
+            });
+        }
+    });
+});
